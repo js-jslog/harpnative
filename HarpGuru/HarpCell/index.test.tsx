e7b76e79549b5811395e0c86a1f6b25c75b1b735
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-15 11:22:00 +0000

    Use the testing-library to test the content of a HarpCell

--- HarpGuru/HarpCell/index.test.tsx
+++ HarpGuru/HarpCell/index.test.tsx
@@ -2,8 +2,10 @@ import 'react-native';
 import React from 'react';
 import {HarpCell} from './index';
 
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
-test('A component is rendered', () => {
-  renderer.create(<HarpCell />);
+test('A component is rendered with an "A" text view', () => {
+  const {getByText} = render(<HarpCell />);
+
+  expect(getByText('A')).toBeTruthy();
 });
