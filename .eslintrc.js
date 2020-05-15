d2973450f68c0cb9174aad56e89f61c1ff7f0c8f
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-15 12:35:34 +0000

    Use the same lint config as in the HarpGuru project
    This means that I'm not extending from the @react-native-community file
    anymore which is definitely not ideal, but I'm going to be copying files
    over from HarpGuru directly and it just makes sense for the styling to
    not have to be changed.
    
    Hopefully I will be given sufficient assistance from the tests which I
    write to spot when something severely illfitting for native has been used.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,74 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-};
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended'
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint',
+        'import',
+        'jest'
+    ],
+    'rules': {
+        'eol-last': ['error', 'always' ],
+        'no-multiple-empty-lines': ['error', {
+            'max': 2,
+            'maxEOF': 0
+        }],
+        'prefer-destructuring': ['error', {
+            'array': true,
+            'object': true
+        }, {
+            'enforceForRenamedProperties': true
+        }],
+        'import/order': ['error', {
+            'alphabetize': { order: 'desc' },
+            'pathGroups': [
+                {
+                  'pattern': './useStyles',
+                  'group': 'index'
+                },
+                {
+                  'pattern': './types',
+                  'group': 'index'
+                },
+                {
+                  'pattern': './constants',
+                  'group': 'index'
+                }
+            ],
+            'newlines-between': 'always'
+        }],
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
