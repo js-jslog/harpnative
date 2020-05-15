60281b0d2f9578955559bcc2fd3c219517de21d2
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-15 15:15:00 +0000

    Add helper function for module to return the top rows

---
+++ HarpGuru/HarpRowsBlow/InteractionMatrixToBlowDrawArray/index.ts
@@ -0,0 +1,10 @@
+import { InteractionIds } from 'harpstrata'
+import type { InteractionRow } from 'harpstrata'
+
+export const mapRowToBlowDrawIds = (row: InteractionRow): InteractionIds | undefined => {
+  const [ firstInteraction ] = row
+  const { id } = firstInteraction || {id: undefined}
+
+  if (id === InteractionIds.Blow || id === InteractionIds.Draw) return id
+  return undefined
+}
