ea6c0648f190c51905f5ab31d74cdd4502785ccc
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-16 21:01:40 +0000

    Add failing test
    The functionality hasn't been created for this test, but I need to move
    to the other room, so premature commitment is required..

--- HarpGuru/HarpGuru/index.test.tsx
+++ HarpGuru/HarpGuru/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { DegreeIds } from 'harpstrata'
-import { render } from '@testing-library/react-native'
+import { PozitionIds, DegreeIds } from 'harpstrata'
+import { render, fireEvent, NativeTestEvent, getAllByText, getByText } from '@testing-library/react-native'
 
 import { HarpGuru } from './index'
 
@@ -12,4 +12,20 @@ test('a component is rendered with a visible Root degree', () => {
 
   expect(root).toBeTruthy()
   expect(controlPanel).toBeTruthy()
+})
+
+test('The HarpFace presents shifting DegreeIds when the relevant Pozitions are selected', () => {
+  const { container } = render(<HarpGuru />)
+  const controlPanel = getByText(container, 'Harp Position')
+
+  fireEvent(getByText(controlPanel, PozitionIds.First), new NativeTestEvent('press'))
+  const [ hole1st ] = getAllByText(container, DegreeIds.Root)
+
+  fireEvent(getByText(controlPanel, PozitionIds.Second), new NativeTestEvent('press'))
+  const hole4th = getByText(hole1st, DegreeIds.Fourth)
+  expect(hole4th).toBeTruthy()
+
+  fireEvent(getByText(controlPanel, PozitionIds.First), new NativeTestEvent('press'))
+  const hole1stAgain = getByText(hole4th, DegreeIds.Root)
+  expect(hole1stAgain).toBeTruthy()
 })
