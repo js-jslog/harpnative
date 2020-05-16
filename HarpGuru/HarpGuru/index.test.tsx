5fc667db0eba39f46268d60b5e50ec2efcd5d625
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-16 13:19:04 +0000

    Move ControlPanel to dedicated folder
    In preparation for sibling components to be created.

--- HarpGuru/HarpGuru/index.test.tsx
+++ HarpGuru/HarpGuru/index.test.tsx
@@ -5,8 +5,11 @@ import { render } from '@testing-library/react-native'
 import { HarpGuru } from './index'
 
 test('a component is rendered with a visible Root degree', () => {
-  const { getAllByText } = render(<HarpGuru />)
+  const { getAllByText, getByText } = render(<HarpGuru />)
 
   const [ root ] = getAllByText(DegreeIds.Root)
+  const controlPanel = getByText('Control Panel')
+
   expect(root).toBeTruthy()
+  expect(controlPanel).toBeTruthy()
 })
