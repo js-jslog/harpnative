3ebfeee26fd5a717d0ad306c2bbf91257e0d0859
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-16 20:48:43 +0000

    Add PozitionButtons to ControlPanel

--- HarpGuru/ControlPanel/ControlPanel/index.test.tsx
+++ HarpGuru/ControlPanel/ControlPanel/index.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { render } from '@testing-library/react-native'
+import { PozitionIds } from 'harpstrata'
+import { render, fireEvent, NativeTestEvent } from '@testing-library/react-native'
 
+import type { ControlPanelProps } from './types'
 import { ControlPanel } from './index'
 
-test('a component is rendered', () => {
-  const container = render(<ControlPanel />)
 
-  expect(container).toBeTruthy()
+test('ControlPanel has a first Pozition control which calls it\'s parameterised function with it\'s expected identity when clicked', () => {
+  const setPozitionId = jest.fn()
+  const controlPanelProps: ControlPanelProps = { setPozitionId }
+  const { getByText } = render(<ControlPanel {...controlPanelProps} />)
+
+  expect(getByText(PozitionIds.First)).toBeTruthy()
+
+  fireEvent(getByText(PozitionIds.First), new NativeTestEvent('press'))
+
+  expect(setPozitionId.mock.calls.length).toBe(1)
+  expect(setPozitionId.mock.calls[0][0]).toBe(PozitionIds.First)
 })
--- HarpGuru/ControlPanel/ControlPanel/index.tsx
+++ HarpGuru/ControlPanel/ControlPanel/index.tsx
@@ -2,14 +2,24 @@ import {StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
 
+import { PozitionButtons } from '../PozitionButtons'
+import type { PozitionButtonParentProps } from '../PozitionButton'
+
+import type { ControlPanelProps } from './types'
+
 const styles = StyleSheet.create({
   controlPanel: {
     flex: 1,
   },
 })
 
-export const ControlPanel = (): ReactElement => (
-  <View style={styles.controlPanel}>
-    <Text>Control Panel</Text>
-  </View>
-)
+export const ControlPanel = (props: ControlPanelProps): ReactElement => {
+  const { setPozitionId } = props
+  const pozitionButtonParentProps: PozitionButtonParentProps = { setPozitionId }
+  return (
+    <View style={styles.controlPanel}>
+      <Text>Control Panel</Text>
+      <PozitionButtons {...pozitionButtonParentProps} />
+    </View>
+  )
+}
