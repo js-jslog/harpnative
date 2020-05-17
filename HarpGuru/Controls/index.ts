003581ffa97eaa72115635e89da98709b863abac
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-17 17:12:22 +0000

    Rename PozitionButtonRich to replace PozitionButton

--- HarpGuru/Controls/index.ts
+++ HarpGuru/Controls/index.ts
@@ -4,5 +4,5 @@ export type { DisplayModeTogglerProps } from './DisplayModeToggler'
 export { PitchButton } from './PitchButton'
 export type { PitchButtonProps } from './PitchButton'
 
-export { PozitionButton } from './PozitionButtonRich'
-export type { PozitionButtonProps } from './PozitionButtonRich'
+export { PozitionButton } from './PozitionButton'
+export type { PozitionButtonProps } from './PozitionButton'
