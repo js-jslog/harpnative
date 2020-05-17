5c9957ee6f960c4dd816a3504ec267909f32b595
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-17 11:47:12 +0000

    Simplify PozitionButtons to clear space in control panel
    I want to get another control in here and then experiment with having
    the control panel on another screen or something.

--- HarpGuru/ControlPanel/PozitionButtons/components/index.tsx
+++ HarpGuru/ControlPanel/PozitionButtons/components/index.tsx
@@ -3,25 +3,16 @@ import React from 'react'
 import type { ReactElement } from 'react'
 import { PozitionIds } from 'harpstrata'
 
-import type { PozitionButtonsProps } from '../types'
+import type { PozitionButtonParentProps } from '../../PozitionButton'
 import { PozitionButton } from '../../PozitionButton'
 import type { PozitionButtonProps } from '../../PozitionButton'
 
-export function PozitionButtons(props: PozitionButtonsProps): ReactElement {
-  const { First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Tenth, Eleventh, Twelfth } = PozitionIds
+export function PozitionButtons(props: PozitionButtonParentProps): ReactElement {
+  const { First, Second, Third } = PozitionIds
   const { setPozitionId } = props
   const firstPozitionButtonProps: PozitionButtonProps = { id: First, setPozitionId }
   const secondPozitionButtonProps: PozitionButtonProps = { id: Second, setPozitionId }
   const thirdPozitionButtonProps: PozitionButtonProps = { id: Third, setPozitionId }
-  const fourthPozitionButtonProps: PozitionButtonProps = { id: Fourth, setPozitionId }
-  const fifthPozitionButtonProps: PozitionButtonProps = { id: Fifth, setPozitionId }
-  const sixthPozitionButtonProps: PozitionButtonProps = { id: Sixth, setPozitionId }
-  const seventhPozitionButtonProps: PozitionButtonProps = { id: Seventh, setPozitionId }
-  const eighthPozitionButtonProps: PozitionButtonProps = { id: Eighth, setPozitionId }
-  const ninthPozitionButtonProps: PozitionButtonProps = { id: Ninth, setPozitionId }
-  const tenthPozitionButtonProps: PozitionButtonProps = { id: Tenth, setPozitionId }
-  const eleventhPozitionButtonProps: PozitionButtonProps = { id: Eleventh, setPozitionId }
-  const twelfthPozitionButtonProps: PozitionButtonProps = { id: Twelfth, setPozitionId }
 
   return (
     <>
@@ -29,15 +20,6 @@ export function PozitionButtons(props: PozitionButtonsProps): ReactElement {
       <PozitionButton {...firstPozitionButtonProps} />
       <PozitionButton {...secondPozitionButtonProps} />
       <PozitionButton {...thirdPozitionButtonProps} />
-      <PozitionButton {...fourthPozitionButtonProps} />
-      <PozitionButton {...fifthPozitionButtonProps} />
-      <PozitionButton {...sixthPozitionButtonProps} />
-      <PozitionButton {...seventhPozitionButtonProps} />
-      <PozitionButton {...eighthPozitionButtonProps} />
-      <PozitionButton {...ninthPozitionButtonProps} />
-      <PozitionButton {...tenthPozitionButtonProps} />
-      <PozitionButton {...eleventhPozitionButtonProps} />
-      <PozitionButton {...twelfthPozitionButtonProps} />
     </>
   )
 }
