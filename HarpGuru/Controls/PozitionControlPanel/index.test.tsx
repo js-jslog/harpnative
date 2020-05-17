f3dd1a64da1d533e3b648073e6846823d92865cf
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-17 17:44:43 +0000

    Add PozitionButtons to the PozitionControlPanel

--- HarpGuru/Controls/PozitionControlPanel/index.test.tsx
+++ HarpGuru/Controls/PozitionControlPanel/index.test.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { PozitionIds } from 'harpstrata'
 import { render } from '@testing-library/react-native'
+
+import { exampleHarpStrataControlProps }  from '../testResources'
 
 import { PozitionControlPanel } from './index'
 
-test('A component is rendered', () => {
-  const component = render(<PozitionControlPanel />)
+test('A component is rendered with a first pozition button present', () => {
+  const { getByText } = render(<PozitionControlPanel {...exampleHarpStrataControlProps} />)
 
-  expect(component).toBeTruthy()
+  expect(getByText(PozitionIds.First)).toBeTruthy()
 })
--- HarpGuru/Controls/PozitionControlPanel/index.tsx
+++ HarpGuru/Controls/PozitionControlPanel/index.tsx
@@ -1,10 +1,21 @@
 import {View} from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
+import { PozitionIds } from 'harpstrata'
 
-export const PozitionControlPanel = (): ReactElement => {
+import { HarpStrataControlProps } from '../types'
+import { PozitionButton } from '../PozitionButton'
+
+export const PozitionControlPanel = (props: HarpStrataControlProps): ReactElement => {
+  const { activeHarpStrata, setActiveHarpStrata } = props
+
+  const firstPozitionButtonProps = { id: PozitionIds.First, activeHarpStrata, setActiveHarpStrata }
+  const secondPozitionButtonProps = { id: PozitionIds.Second, activeHarpStrata, setActiveHarpStrata }
+
   return (
     <View>
+      <PozitionButton {...firstPozitionButtonProps} />
+      <PozitionButton {...secondPozitionButtonProps} />
     </View>
   )
 }
