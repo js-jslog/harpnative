7503b0db99abd3ed029a2f982cb54ec0fde529bc
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-17 13:01:08 +0000

    Simplify test using rerender

--- HarpGuru/HarpFace/HarpCell/index.test.tsx
+++ HarpGuru/HarpFace/HarpCell/index.test.tsx
@@ -7,16 +7,13 @@ import { exampleHarpFaceProps } from '../HarpFace'
 
 import {HarpCell} from './index'
 
-test('A component is rendered with the Degree value in its text view when the Degree DisplayMode is selected', () => {
+test('A component is rendered with the Degree or Pitch value in its text view depending on the DisplayMode selected', () => {
   const harpFaceProps = { ...exampleHarpFaceProps, displayMode: DisplayModes.Degree }
-  const {getByText} = render(<HarpCell {...harpFaceProps} yxCoord={[3,0]} />)
+  const {getByText, rerender} = render(<HarpCell {...harpFaceProps} yxCoord={[3,0]} />)
 
   expect(getByText(DegreeIds.Second)).toBeTruthy()
-})
 
-test('A component is rendered with the Pitch value in its text view when the Pitch DisplayMode is selected', () => {
-  const harpFaceProps = { ...exampleHarpFaceProps, displayMode: DisplayModes.Pitch }
-  const {getByText} = render(<HarpCell {...harpFaceProps} yxCoord={[3,0]} />)
+  rerender(<HarpCell {...harpFaceProps} displayMode={DisplayModes.Pitch} yxCoord={[3,0]} />)
 
   expect(getByText(PitchIds.D)).toBeTruthy()
 })
