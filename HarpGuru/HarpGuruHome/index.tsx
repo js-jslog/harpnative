8a940c202e9b6a2cdd0ebe6173f29dbd0fd37855
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-18 06:34:23 +0000

    Add styling in to HarpGuruHome

--- HarpGuru/HarpGuruHome/index.tsx
+++ HarpGuru/HarpGuruHome/index.tsx
@@ -1,4 +1,4 @@
-import {View} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import React from 'react'
 
 import type {HarpFaceProps} from '../HarpFace'
@@ -8,13 +8,23 @@ import type { DisplayModeTogglerProps } from '../Controls'
 
 type HarpGuruHomeProps = HarpFaceProps & DisplayModeTogglerProps
 
+const styles = StyleSheet.create({
+  guruhome: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+  },
+})
+
 
 export const HarpGuruHome = (props: HarpGuruHomeProps): React.ReactElement => {
   const { harpStrata, displayMode, setDisplayMode } = props
   const harpFaceProps = { harpStrata, displayMode }
   const displayModeTogglerProps = { setDisplayMode }
   return (
-    <View>
+    <View style={styles.guruhome}>
       <HarpFace {...harpFaceProps} />
       <DisplayModeToggler {...displayModeTogglerProps} />
     </View>
