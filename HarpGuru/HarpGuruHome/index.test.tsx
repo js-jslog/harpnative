c5a27d2e99f4b4f6823b2045f32ad187bd7abdb5
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-18 06:31:45 +0000

    Add test and fix broken associated functionality

--- HarpGuru/HarpGuruHome/index.test.tsx
+++ HarpGuru/HarpGuruHome/index.test.tsx
@@ -1,7 +1,8 @@
 import 'react-native'
 import React from 'react'
-import {render} from '@testing-library/react-native'
+import {render, fireEvent, NativeTestEvent} from '@testing-library/react-native'
 
+import { DisplayModes } from '../HarpFace'
 import { exampleHarpFaceProps } from '../HarpFace'
 
 import {HarpGuruHome} from './index'
@@ -12,4 +13,17 @@ test('A component is rendered', () => {
   const { container } = render(<HarpGuruHome {...harpGuruHomeProps} />)
 
   expect(container).toBeTruthy()
+})
+
+test('The HarpGuruHome calls the parameterised setDisplayMode function when the degree and pich buttons are pressed', () => {
+  const setDisplayMode = jest.fn()
+  const harpGuruHomeProps = { ...exampleHarpFaceProps, setDisplayMode }
+  const { getByText } = render(<HarpGuruHome { ...harpGuruHomeProps } />)
+
+  fireEvent(getByText(DisplayModes.Degree), new NativeTestEvent('press'))
+  fireEvent(getByText(DisplayModes.Pitch), new NativeTestEvent('press'))
+
+  expect(setDisplayMode.mock.calls.length).toBe(2)
+  expect(setDisplayMode.mock.calls[0][0]).toBe(DisplayModes.Degree)
+  expect(setDisplayMode.mock.calls[1][0]).toBe(DisplayModes.Pitch)
 })
--- HarpGuru/HarpGuruHome/index.tsx
+++ HarpGuru/HarpGuruHome/index.tsx
@@ -1,11 +1,17 @@
 import {View} from 'react-native'
 import React from 'react'
 
-import type {HarpFaceProps, DisplayModes} from '../HarpFace'
+import type {HarpFaceProps} from '../HarpFace'
 import { HarpFace } from '../HarpFace'
 import { DisplayModeToggler } from '../Controls'
+import type { DisplayModeTogglerProps } from '../Controls'
 
-export const HarpGuruHome = (harpFaceProps: HarpFaceProps, setDisplayMode: (displayMode: DisplayModes) => void): React.ReactElement => {
+type HarpGuruHomeProps = HarpFaceProps & DisplayModeTogglerProps
+
+
+export const HarpGuruHome = (props: HarpGuruHomeProps): React.ReactElement => {
+  const { harpStrata, displayMode, setDisplayMode } = props
+  const harpFaceProps = { harpStrata, displayMode }
   const displayModeTogglerProps = { setDisplayMode }
   return (
     <View>
