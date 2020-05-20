8875454589fc342e274d06afe07c9b4b8fbc806f
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-20 19:06:47 +0000

    Add pitch controls and some rudimentary layout to control panel

--- HarpGuru/Screens/PozitionControl/index.tsx
+++ HarpGuru/Screens/PozitionControl/index.tsx
@@ -1,10 +1,10 @@
-import {View} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
-import { PozitionIds } from 'harpstrata'
+import { PozitionIds, PitchIds } from 'harpstrata'
 
 import type { ScreenProps } from '../types'
-import { PozitionButton, HeadsupDisplay } from '../../Controls'
+import { PozitionButton, PitchButton, HeadsupDisplay } from '../../Controls'
 
 export const PozitionControlScreen = (props: ScreenProps): ReactElement => {
   const { activeHarpStrata, setActiveHarpStrata } = props
@@ -17,16 +17,59 @@ export const PozitionControlScreen = (props: ScreenProps): ReactElement => {
   const sixthPozitionButtonProps = { id: PozitionIds.Sixth, activeHarpStrata, setActiveHarpStrata }
   const seventhPozitionButtonProps = { id: PozitionIds.Seventh, activeHarpStrata, setActiveHarpStrata }
 
+  const cPitchButtonProps = { id: PitchIds.C, activeHarpStrata, setActiveHarpStrata }
+  const dPitchButtonProps = { id: PitchIds.D, activeHarpStrata, setActiveHarpStrata }
+  const ePitchButtonProps = { id: PitchIds.E, activeHarpStrata, setActiveHarpStrata }
+  const fPitchButtonProps = { id: PitchIds.F, activeHarpStrata, setActiveHarpStrata }
+  const gPitchButtonProps = { id: PitchIds.G, activeHarpStrata, setActiveHarpStrata }
+  const aPitchButtonProps = { id: PitchIds.A, activeHarpStrata, setActiveHarpStrata }
+  const bPitchButtonProps = { id: PitchIds.B, activeHarpStrata, setActiveHarpStrata }
+
+
+  const styles = StyleSheet.create({
+    headsupdiplay: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    controls: {
+      flex: 5,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    column: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    },
+  })
+
+
   return (
-    <View>
-      <HeadsupDisplay {...activeHarpStrata} />
-      <PozitionButton {...firstPozitionButtonProps} />
-      <PozitionButton {...secondPozitionButtonProps} />
-      <PozitionButton {...thirdPozitionButtonProps} />
-      <PozitionButton {...fourthPozitionButtonProps} />
-      <PozitionButton {...fifthPozitionButtonProps} />
-      <PozitionButton {...sixthPozitionButtonProps} />
-      <PozitionButton {...seventhPozitionButtonProps} />
-    </View>
+    <>
+      <View style={styles.headsupdiplay}>
+        <HeadsupDisplay {...activeHarpStrata} />
+      </View>
+      <View style={styles.controls}>
+        <View style={styles.column}>
+          <PozitionButton {...firstPozitionButtonProps} />
+          <PozitionButton {...secondPozitionButtonProps} />
+          <PozitionButton {...thirdPozitionButtonProps} />
+          <PozitionButton {...fourthPozitionButtonProps} />
+          <PozitionButton {...fifthPozitionButtonProps} />
+          <PozitionButton {...sixthPozitionButtonProps} />
+          <PozitionButton {...seventhPozitionButtonProps} />
+        </View>
+        <View style={styles.column}>
+          <PitchButton {...cPitchButtonProps} />
+          <PitchButton {...dPitchButtonProps} />
+          <PitchButton {...ePitchButtonProps} />
+          <PitchButton {...fPitchButtonProps} />
+          <PitchButton {...gPitchButtonProps} />
+          <PitchButton {...aPitchButtonProps} />
+          <PitchButton {...bPitchButtonProps} />
+        </View>
+      </View>
+    </>
   )
 }
