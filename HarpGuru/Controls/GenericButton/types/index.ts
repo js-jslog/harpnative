b52ab6260a0e87448b36eb10c95e929809449c4c
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-21 21:11:51 +0000

    Simplify type definitions

--- HarpGuru/Controls/GenericButton/types/index.ts
+++ HarpGuru/Controls/GenericButton/types/index.ts
@@ -1,15 +1,15 @@
 import type { HarpStrata, PozitionIds, PitchIds } from 'harpstrata'
 
-type PozitionUpdateProps = {
+type CommonGenericUpdateProps = {
+  readonly activeHarpStrata: HarpStrata;
+}
+type PozitionUpdateProps = CommonGenericUpdateProps & {
   readonly updateCategory: UpdateCategories.Pozition;
   readonly updateId: PozitionIds;
 }
-type HarpKeyUpdateProps = {
+type HarpKeyUpdateProps = CommonGenericUpdateProps & {
   readonly updateCategory: UpdateCategories.HarpKey;
   readonly updateId: PitchIds;
-}
-type GenericAugmentProps = {
-  readonly activeHarpStrata: HarpStrata;
 }
 
 export enum UpdateCategories {
@@ -17,4 +17,4 @@ export enum UpdateCategories {
   HarpKey = 'HARP_KEY',
 }
 
-export type GenericUpdateProps = ( PozitionUpdateProps & GenericAugmentProps ) | ( HarpKeyUpdateProps & GenericAugmentProps )
+export type GenericUpdateProps = PozitionUpdateProps | HarpKeyUpdateProps
