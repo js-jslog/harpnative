a71a67a184e7758d248ec013c1fdf8a657edcddc
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-21 21:56:39 +0000

    Add setActiveHarpStrata to Generic funcionality

--- HarpGuru/Controls/GenericUpdateButton/components/index.test.tsx
+++ HarpGuru/Controls/GenericUpdateButton/components/index.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { PitchIds } from 'harpstrata'
-import { render } from '@testing-library/react-native'
+import { render, fireEvent, NativeTestEvent } from '@testing-library/react-native'
 
 import { UpdateCategories } from '../types'
 import type { GenericUpdateProps } from '../types'
-import { keyCHarpStrata } from '../testResources'
+import { keyCHarpStrata, keyDHarpStrata } from '../testResources'
 
 import { GenericUpdateButton } from './index'
 
-test('GenericButton renders a component with the parameter label on it', () => {
+test('GenericUpdateButton renders a component with the parameter label on it', () => {
   const activeHarpStrata = keyCHarpStrata
   const setActiveHarpStrata = jest.fn()
   const { HarpKey: updateCategory } = UpdateCategories
@@ -21,4 +21,22 @@ test('GenericButton renders a component with the parameter label on it', () => {
   const { getByText } = render(<GenericUpdateButton {...harpKeyUpdateProps} />)
 
   expect(getByText(updateId)).toBeTruthy()
+})
+
+test('GenericUpdateButton renders a component which set\'s the expected harp strata to the paramaterised function', () => {
+  const setActiveHarpStrata = jest.fn()
+  const activeHarpStrata = keyDHarpStrata
+  const { HarpKey: updateCategory } = UpdateCategories
+  const { C: updateId } = PitchIds
+
+  const harpKeyUpdateProps: GenericUpdateProps = {
+    activeHarpStrata, setActiveHarpStrata, updateCategory, updateId
+  }
+
+  const { getByText } = render(<GenericUpdateButton {...harpKeyUpdateProps} />)
+
+  fireEvent(getByText(updateId), new NativeTestEvent('press'))
+
+  expect(setActiveHarpStrata.mock.calls.length).toBe(1)
+  expect(setActiveHarpStrata.mock.calls[0][0]).toStrictEqual(keyCHarpStrata)
 })
--- HarpGuru/Controls/GenericUpdateButton/components/index.tsx
+++ HarpGuru/Controls/GenericUpdateButton/components/index.tsx
@@ -1,11 +1,19 @@
 import {Button} from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
+import { getHarpStrata } from 'harpstrata'
 
 import type { GenericUpdateProps } from '../types'
+import { getUpdateHarpStrataProps } from '../getUpdateHarpStrataProps'
+
+const setNewHarpStrata = (props: GenericUpdateProps): void => {
+  const { setActiveHarpStrata } = props
+  const newHarpStrata = getHarpStrata(getUpdateHarpStrataProps(props))
+  setActiveHarpStrata(newHarpStrata)
+}
 
 export function GenericUpdateButton(props: GenericUpdateProps): ReactElement {
   return (
-    <Button onPress={(): void => {1 + 2}} title={props.updateId} disabled={false} />
+    <Button onPress={(): void => {setNewHarpStrata(props)}} title={props.updateId} disabled={false} />
   )
 }
