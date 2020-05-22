a95126701d8d776f79c9ddc7338ab070e02f69ba
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 13:29:29 +0000

    Add tests to helper function

--- HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.test.ts
+++ HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.test.ts
@@ -2,8 +2,12 @@ import { exampleHarpFaceProps } from '../../HarpFace'
 
 import { isBlowOrDrawRow } from './index'
 
-test('isBlowOrDrawRow returns true for a blow row', () => {
+test('isBlowOrDrawRow returns true for a blow or draw row and false otherwise', () => {
   const harpRowPropsBlow = { ...exampleHarpFaceProps, yCoord: 2 }
+  const harpRowPropsDraw = { ...exampleHarpFaceProps, yCoord: 3 }
+  const harpRowPropsBend = { ...exampleHarpFaceProps, yCoord: 4 }
   
   expect(isBlowOrDrawRow(harpRowPropsBlow)).toBeTruthy()
+  expect(isBlowOrDrawRow(harpRowPropsDraw)).toBeTruthy()
+  expect(isBlowOrDrawRow(harpRowPropsBend)).toBeFalsy()
 })
