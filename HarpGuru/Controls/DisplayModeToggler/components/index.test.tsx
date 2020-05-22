ff5f7e8809e16dde2c2dc2d4ac507349e1a222ee
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 08:32:25 +0000

    Add test for Pitch toggle as well as Degree

--- HarpGuru/Controls/DisplayModeToggler/components/index.test.tsx
+++ HarpGuru/Controls/DisplayModeToggler/components/index.test.tsx
@@ -34,3 +34,14 @@ test('DisplayModeToggler Degree button calls a paramaterised function with it\'s
   expect(setActiveDisplayMode.mock.calls.length).toBe(1)
   expect(setActiveDisplayMode.mock.calls[0][0]).toBe(DisplayModes.Degree)
 })
+
+test('DisplayModeToggler Pitch button calls a paramaterised function with it\'s identity when it is clicked', () => {
+  const setActiveDisplayMode = jest.fn()
+  const displayModeTogglerProps: DisplayModeTogglerProps = { setActiveDisplayMode }
+  const { getByA11yLabel } = render(<DisplayModeToggler {...displayModeTogglerProps} />)
+
+  fireEvent.press(getByA11yLabel(DisplayModes.Pitch))
+
+  expect(setActiveDisplayMode.mock.calls.length).toBe(1)
+  expect(setActiveDisplayMode.mock.calls[0][0]).toBe(DisplayModes.Pitch)
+})
