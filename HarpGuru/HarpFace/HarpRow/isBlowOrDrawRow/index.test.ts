340ec3a9f32d3f27fcf7eb65cecfe4c7b3daec58
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 13:48:30 +0000

    Add individual test for isBlowRow

--- HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.test.ts
+++ HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.test.ts
@@ -1,6 +1,16 @@
 import { exampleHarpFaceProps } from '../../HarpFace'
 
-import { isBlowOrDrawRow } from './index'
+import { isBlowOrDrawRow, isBlowRow } from './index'
+
+test('isBlowRow returns true for a blow row and false otherwise', () => {
+  const harpRowPropsBlow = { ...exampleHarpFaceProps, yCoord: 2 }
+  const harpRowPropsDraw = { ...exampleHarpFaceProps, yCoord: 3 }
+  const harpRowPropsBend = { ...exampleHarpFaceProps, yCoord: 4 }
+  
+  expect(isBlowRow(harpRowPropsBlow)).toBeTruthy()
+  expect(isBlowRow(harpRowPropsDraw)).toBeFalsy()
+  expect(isBlowRow(harpRowPropsBend)).toBeFalsy()
+})
 
 test('isBlowOrDrawRow returns true for a blow or draw row and false otherwise', () => {
   const harpRowPropsBlow = { ...exampleHarpFaceProps, yCoord: 2 }
--- HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.ts
+++ HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.ts
@@ -2,6 +2,17 @@ import { InteractionIds } from 'harpstrata'
 
 import type { HarpRowProps } from '../types'
 
+
+export const isBlowRow = (props: HarpRowProps): boolean => {
+  const { yCoord, activeHarpStrata } = props
+  const { apparatus: {interactionMatrix }} = activeHarpStrata
+  const { [yCoord]: {[0]: thisInteraction} } = interactionMatrix
+  const { id: interactionId } = thisInteraction || { id: undefined }
+  const isBlow = (interactionId === InteractionIds.Blow)
+
+  return isBlow
+}
+
 export const isBlowOrDrawRow = (props: HarpRowProps): boolean => {
   const { yCoord, activeHarpStrata } = props
   const { apparatus: {interactionMatrix }} = activeHarpStrata
