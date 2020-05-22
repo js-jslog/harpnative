c1b593992934414affced3446b7f6a0833c9410f
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 13:51:22 +0000

    Remove function redundancy
    Sure this will be a little slower, but that really doesn't matter.

--- HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.ts
+++ HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.ts
@@ -24,12 +24,5 @@ export const isDrawRow = (props: HarpRowProps): boolean => {
 }
 
 export const isBlowOrDrawRow = (props: HarpRowProps): boolean => {
-  const { yCoord, activeHarpStrata } = props
-  const { apparatus: {interactionMatrix }} = activeHarpStrata
-  const { [yCoord]: {[0]: thisInteraction} } = interactionMatrix
-  const { id: interactionId } = thisInteraction || { id: undefined }
-  const isBlow = (interactionId === InteractionIds.Blow)
-  const isDraw = (interactionId === InteractionIds.Draw)
-
-  return (isBlow || isDraw)
+  return (isBlowRow(props) || isDrawRow(props))
 }
