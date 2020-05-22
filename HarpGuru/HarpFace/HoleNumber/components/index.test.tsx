6ece852fe327104ca325e7ca3fe4d1e02b62f172
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 08:21:57 +0000

    Create HoleNumbers enum

--- HarpGuru/HarpFace/HoleNumber/components/index.test.tsx
+++ HarpGuru/HarpFace/HoleNumber/components/index.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { render } from '@testing-library/react-native'
 
+import { HoleNumbers } from '../types'
+
 import { HoleNumber } from './index'
 
 test('HoleNumber renders a dom element with the expected value included', () => {
   const { getByText } = render(<HoleNumber xCoord={0} />)
-  expect(getByText('1')).toBeTruthy()
+  expect(getByText(HoleNumbers.One)).toBeTruthy()
 })
--- HarpGuru/HarpFace/HoleNumber/components/index.tsx
+++ HarpGuru/HarpFace/HoleNumber/components/index.tsx
@@ -2,11 +2,11 @@ import {StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
 
-import type { HoleNumberProps } from '../types'
+import type { HoleNumbers, HoleNumberProps } from '../types'
 
 export function HoleNumber(props: HoleNumberProps): ReactElement {
   const { xCoord } = props
-  const holeNumber = xCoord + 1
+  const holeNumber: HoleNumbers = `${xCoord + 1}` as HoleNumbers
 
   const styles = StyleSheet.create({
     cell: {
