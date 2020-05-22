0d5152a314e34bdd0909dadf0c5713189c5d5855
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 13:50:14 +0000

    Add individual test for isDrawRow

--- HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.test.ts
+++ HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.test.ts
@@ -1,6 +1,6 @@
 import { exampleHarpFaceProps } from '../../HarpFace'
 
-import { isBlowOrDrawRow, isBlowRow } from './index'
+import { isBlowOrDrawRow, isBlowRow, isDrawRow } from './index'
 
 test('isBlowRow returns true for a blow row and false otherwise', () => {
   const harpRowPropsBlow = { ...exampleHarpFaceProps, yCoord: 2 }
@@ -10,6 +10,16 @@ test('isBlowRow returns true for a blow row and false otherwise', () => {
   expect(isBlowRow(harpRowPropsBlow)).toBeTruthy()
   expect(isBlowRow(harpRowPropsDraw)).toBeFalsy()
   expect(isBlowRow(harpRowPropsBend)).toBeFalsy()
+})
+
+test('isDrawRow returns true for a blow row and false otherwise', () => {
+  const harpRowPropsBlow = { ...exampleHarpFaceProps, yCoord: 2 }
+  const harpRowPropsDraw = { ...exampleHarpFaceProps, yCoord: 3 }
+  const harpRowPropsBend = { ...exampleHarpFaceProps, yCoord: 4 }
+  
+  expect(isDrawRow(harpRowPropsBlow)).toBeFalsy()
+  expect(isDrawRow(harpRowPropsDraw)).toBeTruthy()
+  expect(isDrawRow(harpRowPropsBend)).toBeFalsy()
 })
 
 test('isBlowOrDrawRow returns true for a blow or draw row and false otherwise', () => {
--- HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.ts
+++ HarpGuru/HarpFace/HarpRow/isBlowOrDrawRow/index.ts
@@ -13,6 +13,16 @@ export const isBlowRow = (props: HarpRowProps): boolean => {
   return isBlow
 }
 
+export const isDrawRow = (props: HarpRowProps): boolean => {
+  const { yCoord, activeHarpStrata } = props
+  const { apparatus: {interactionMatrix }} = activeHarpStrata
+  const { [yCoord]: {[0]: thisInteraction} } = interactionMatrix
+  const { id: interactionId } = thisInteraction || { id: undefined }
+  const isDraw = (interactionId === InteractionIds.Draw)
+
+  return isDraw
+}
+
 export const isBlowOrDrawRow = (props: HarpRowProps): boolean => {
   const { yCoord, activeHarpStrata } = props
   const { apparatus: {interactionMatrix }} = activeHarpStrata
