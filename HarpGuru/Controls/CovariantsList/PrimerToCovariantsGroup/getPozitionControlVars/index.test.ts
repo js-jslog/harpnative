d68b28268cc893a76bf8a04bae64be0d3d17954a
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-27 21:15:50 +0000

    Add test of other PoztionControlVars getter orientation

--- HarpGuru/Controls/CovariantsList/PrimerToCovariantsGroup/getPozitionControlVars/index.test.ts
+++ HarpGuru/Controls/CovariantsList/PrimerToCovariantsGroup/getPozitionControlVars/index.test.ts
@@ -20,3 +20,19 @@ test('getPozitionControlVars returns an array of PozitionControlVars for a locke
 
   expect(actualControlVars).toEqual(expectedControlVars)
 })
+
+test('getPozitionControlVars returns an array of PozitionControlVars for a locked root pitch', () => {
+  const { RootPitch: lockedType } = CovariantTypes
+  const { HarpKey: variedType } = CovariantTypes
+  const { Eb: lockedValue } = PitchIds
+  const { A: variedValue } = PitchIds
+
+  const expectedControlVars: PozitionControlVars = {
+    harpKeyId: PitchIds.A,
+    rootPitchId: PitchIds.Eb,
+  }
+
+  const actualControlVars = getPozitionControlVars({lockedType, variedType, lockedValue, variedValue})
+
+  expect(actualControlVars).toEqual(expectedControlVars)
+})
