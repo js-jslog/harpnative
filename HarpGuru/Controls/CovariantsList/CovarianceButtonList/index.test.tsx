683390e89d9f3a3c0d355131e28bd2784023dd7c
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-29 13:18:40 +0000

    Add a naively produced button to the list

--- HarpGuru/Controls/CovariantsList/CovarianceButtonList/index.test.tsx
+++ HarpGuru/Controls/CovariantsList/CovarianceButtonList/index.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { PitchIds, PozitionIds } from 'harpstrata'
 import { render } from '@testing-library/react-native'
+
+import { keyCHarpStrata } from '../testResources'
 
 import { CovarianceButtonList } from './index'
 
-test('A list is produced with a CovarianceButton in it', () => {
-  const component = render(<CovarianceButtonList />)
+test('A list is produced with a RootPitchControlVars CovarianceButton in it', () => {
+  const harpStrataControlProps = {
+    setActiveHarpStrata: jest.fn(),
+    activeHarpStrata: keyCHarpStrata, 
+  }
+  const { getByText } = render(<CovarianceButtonList {...harpStrataControlProps} />)
 
-  expect(component).toBeTruthy()
+  const expectedText = `${PitchIds.C} ${PozitionIds.First}`
+  expect(getByText(expectedText)).toBeTruthy()
 })
--- HarpGuru/Controls/CovariantsList/CovarianceButtonList/index.tsx
+++ HarpGuru/Controls/CovariantsList/CovarianceButtonList/index.tsx
@@ -1,9 +1,19 @@
-import { View } from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
+import { PozitionIds, PitchIds } from 'harpstrata'
+import type { RootPitchControlVars } from 'harpstrata'
 
-export const CovarianceButtonList = (): ReactElement => {
+import { CovarianceButton } from '../CovarianceButton'
+import { HarpStrataControlProps } from '../../types'
+
+export const CovarianceButtonList = (props: HarpStrataControlProps): ReactElement => {
+  const rootPitchControlVars: RootPitchControlVars = {
+    harpKeyId: PitchIds.C,
+    pozitionId: PozitionIds.First,
+  }
+  const covarianceButtonProps = { ...props, covariantControlVars: rootPitchControlVars }
+
   return (
-    <View />
+    <CovarianceButton {...covarianceButtonProps} />
   )
 }
