8730663c99828920c7aaac821305c1841d2ecb0b
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-29 15:38:08 +0000

    Alter test due to flawed dependency
    The ordering of the PozitionIds is not satisfactory at the moment since
    they are ordered by root offset rather than by name. The ordering by
    offset will emerge when either of the pitch types are the variedType so
    thats unnecesary and will be fixed. When it gets fixed I don't want to
    have to rewrite this test.

--- HarpGuru/Controls/CovarianceButtonList/PrimerToCovariantsGroup/getCovariantsGroupList/index.test.ts
+++ HarpGuru/Controls/CovarianceButtonList/PrimerToCovariantsGroup/getCovariantsGroupList/index.test.ts
@@ -8,17 +8,19 @@ import { getCovariantsGroupList } from './index'
 
 test('getCovariantsGroupList returns a single entry representing the first CovariantsGroup in the list', () => {
   const controlVarsPrimer: ControlVarsPrimer = {
-    lockedType: CovariantTypes.HarpKey,
-    variedType: CovariantTypes.Pozition,
-    lockedValue: PitchIds.C,
-    variedValue: PozitionIds.First,
+    lockedType: CovariantTypes.Pozition,
+    variedType: CovariantTypes.HarpKey,
+    lockedValue: PozitionIds.First,
+    variedValue: PitchIds.C,
   }
 
-  const expectedCovariantsGroupList: ReadonlyArray<CovariantsGroup> = [{
-    harpKeyId: PitchIds.C,
-    pozitionId: PozitionIds.First,
-    rootPitchId: PitchIds.C,
-  }]
+  const expectedCovariantsGroupList: ReadonlyArray<CovariantsGroup> = [
+    {
+      harpKeyId: PitchIds.C,
+      pozitionId: PozitionIds.First,
+      rootPitchId: PitchIds.C,
+    }
+  ]
 
   const actualCovariantsGroupList = getCovariantsGroupList(controlVarsPrimer)
 
