c6de24adc21905b1fafd0f861398587f61b6ad5b
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-29 10:47:05 +0000

    Implement different labels for different control vars
    This has required the inclusion of some typeguards which already exist
    in harpstrata. I should make a change to harpstrata to export those
    typeguards rather than develop them again here.

--- HarpGuru/Controls/CovariantsList/CovarianceButton/index.test.tsx
+++ HarpGuru/Controls/CovariantsList/CovarianceButton/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PitchIds, PozitionIds } from 'harpstrata'
-import type { RootPitchControlVars } from 'harpstrata'
+import type { HarpKeyControlVars, RootPitchControlVars } from 'harpstrata'
 import { render } from '@testing-library/react-native'
 
 import { keyCHarpStrata } from '../testResources'
@@ -8,7 +8,7 @@ import { keyCHarpStrata } from '../testResources'
 import { CovarianceButton } from './index'
 
 
-test('Displays a Pozition and a Harp Key when a RootPitchControlVars object is given', () => {
+test('Displays a Harp Key and Pozition when a RootPitchControlVars object is given', () => {
   const setActiveHarpStrata = jest.fn()
   const activeHarpStrata = keyCHarpStrata
   const covariantControlVars: RootPitchControlVars = {
@@ -21,5 +21,21 @@ test('Displays a Pozition and a Harp Key when a RootPitchControlVars object is g
   const { getByText } = render(<CovarianceButton { ...covarianceButtonProps } />)
 
   const expectedTitle = `${PitchIds.C} ${PozitionIds.Second}`
+  expect(getByText(expectedTitle)).toBeTruthy()
+})
+
+test('Displays a Pozition and a Root Pitch when a HarpKeyControlVars object is given', () => {
+  const setActiveHarpStrata = jest.fn()
+  const activeHarpStrata = keyCHarpStrata
+  const covariantControlVars: HarpKeyControlVars = {
+    pozitionId: PozitionIds.Second,
+    rootPitchId: PitchIds.C,
+  }
+
+  const covarianceButtonProps = { setActiveHarpStrata, activeHarpStrata, covariantControlVars }
+
+  const { getByText } = render(<CovarianceButton { ...covarianceButtonProps } />)
+
+  const expectedTitle = `${PozitionIds.Second} ${PitchIds.C}`
   expect(getByText(expectedTitle)).toBeTruthy()
 })
--- HarpGuru/Controls/CovariantsList/CovarianceButton/index.tsx
+++ HarpGuru/Controls/CovariantsList/CovarianceButton/index.tsx
@@ -1,15 +1,44 @@
 import { Button } from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
-import type { RootPitchControlVars } from 'harpstrata'
+import type { CovariantControlVars, HarpKeyControlVars, PozitionControlVars, RootPitchControlVars } from 'harpstrata'
 
 import { CovarianceButtonProps } from './types'
 
+export const isHarpKeyControlVars = (props: CovariantControlVars): props is HarpKeyControlVars => {
+  const hasRootPitch = (props as HarpKeyControlVars).rootPitchId !== undefined
+  const hasPozition = (props as HarpKeyControlVars).pozitionId !== undefined
+
+  return hasRootPitch && hasPozition
+}
+export const isPozitionControlVars = (props: CovariantControlVars): props is PozitionControlVars => {
+  const hasRootPitch = (props as PozitionControlVars).rootPitchId !== undefined
+  const hasHarpKey = (props as PozitionControlVars).harpKeyId !== undefined
+
+  return hasRootPitch && hasHarpKey
+}
+export const isRootPitchControlVars = (props: CovariantControlVars): props is RootPitchControlVars => {
+  const hasHarpKey = (props as RootPitchControlVars).harpKeyId !== undefined
+  const hasPozition = (props as RootPitchControlVars).pozitionId !== undefined
+
+  return hasHarpKey && hasPozition
+}
+
 export const CovarianceButton = (props: CovarianceButtonProps): ReactElement => {
-  const { covariantControlVars: rootPitchControlVars } = props
-  const { harpKeyId, pozitionId } = rootPitchControlVars as RootPitchControlVars 
-  const title = `${harpKeyId} ${pozitionId}`
-  return (
-    <Button onPress={(): void => {console.log('test')}} title={title} />
-  )
+  const { covariantControlVars } = props
+  if (isRootPitchControlVars(covariantControlVars)) {
+    const { harpKeyId, pozitionId } = covariantControlVars
+    const title = `${harpKeyId} ${pozitionId}`
+    return (
+      <Button onPress={(): void => {console.log('test')}} title={title} />
+    )
+  } else if (isHarpKeyControlVars(covariantControlVars)) {
+    const { pozitionId, rootPitchId } = covariantControlVars
+    const title = `${pozitionId} ${rootPitchId}`
+    return (
+      <Button onPress={(): void => {console.log('test')}} title={title} />
+    )
+  }
+
+  throw Error ('harp key control vars not implemented yet')
 }
