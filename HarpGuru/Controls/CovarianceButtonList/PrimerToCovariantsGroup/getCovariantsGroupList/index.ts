aef751bb85f7d7b5e644ff904fe65b0c4493a955
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-01 20:44:14 +0000

    Employ helper function to reduce complexity

--- HarpGuru/Controls/CovarianceButtonList/PrimerToCovariantsGroup/getCovariantsGroupList/index.ts
+++ HarpGuru/Controls/CovarianceButtonList/PrimerToCovariantsGroup/getCovariantsGroupList/index.ts
@@ -1,43 +1,12 @@
-import { getPitchIds, PitchIds, getCovariants, CovariantsGroup } from 'harpstrata'
-import type { CovariantControlVars, RootPitchControlVars, PozitionControlVars } from 'harpstrata'
+import { getCovariants, CovariantsGroup } from 'harpstrata'
 
 import type { ControlVarsPrimer } from '../types'
-import { getControlVars } from '../getControlVars'
-
-const isPozitionControlVars = (props: CovariantControlVars): props is PozitionControlVars => {
-  const hasRootPitch = (props as PozitionControlVars).rootPitchId !== undefined
-  const hasHarpKey = (props as PozitionControlVars).harpKeyId !== undefined
-
-  return hasRootPitch && hasHarpKey
-}
-const isRootPitchControlVars = (props: CovariantControlVars): props is RootPitchControlVars => {
-  const hasHarpKey = (props as RootPitchControlVars).harpKeyId !== undefined
-  const hasPozition = (props as RootPitchControlVars).pozitionId !== undefined
-
-  return hasHarpKey && hasPozition
-}
+import { getControlVarsArray } from '../getControlVarsArray'
 
 export const getCovariantsGroupList = (props: ControlVarsPrimer): ReadonlyArray<CovariantsGroup> => {
-  const controlVars = getControlVars(props)
-  if (isRootPitchControlVars(controlVars)) {
-    const { variedValue } = props
-    if (Object.values(PitchIds).includes(variedValue as PitchIds)) {
-      const variedArray = getPitchIds(variedValue as PitchIds)
-      const covariantsGroupList = variedArray.map((harpKeyId) => {
-        return getCovariants({ ...controlVars, harpKeyId })
-      })
-      return covariantsGroupList
-    }
-  } else if (isPozitionControlVars(controlVars)) {
-    const { variedValue } = props
-    if (Object.values(PitchIds).includes(variedValue as PitchIds)) {
-      const variedArray = getPitchIds(variedValue as PitchIds)
-      const covariantsGroupList = variedArray.map((harpKeyId) => {
-        return getCovariants({ ...controlVars, harpKeyId })
-      })
-      return covariantsGroupList
-    }
-  }
-
-  throw new Error ('Function not set up to handle HarpKeyControlVars at the moment')
+  const controlVarsArray = getControlVarsArray(props)
+  
+  return controlVarsArray.map((controlVars) => {
+    return getCovariants(controlVars)
+  })
 }
