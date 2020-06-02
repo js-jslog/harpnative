0dd677d22e92752264ba2e21eac90f34e89167fb
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-02 21:57:22 +0000

    Export the complete list of primer vars

--- HarpGuru/Controls/CovariantsGroupList/index.ts
+++ HarpGuru/Controls/CovariantsGroupList/index.ts
@@ -1,6 +1,9 @@
 export { getCovariantsGroupList } from './getCovariantsGroupList'
-export type { ControlVarsPrimer } from './types'
-
-// TODO: need to export all of the types here
-export type { PozitionControlPrimerLockedRootPitch, HarpKeyControlPrimerLockedPozition } from './types'
 export { CovarianceParts } from './types'
+export type {
+  ControlVarsPrimer,
+  HarpKeyControlPrimer, PozitionControlPrimer, RootPitchControlPrimer,
+  HarpKeyControlPrimerLockedPozition, HarpKeyControlPrimerLockedRootPitch,
+  PozitionControlPrimerLockedHarpKey, PozitionControlPrimerLockedRootPitch,
+  RootPitchControlPrimerLockedHarpKey, RootPitchControlPrimerLockedPozition,
+} from './types'
