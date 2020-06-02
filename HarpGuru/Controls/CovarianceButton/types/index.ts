5c36534dedf250d14cdfe511ee956b82d8b9f6bc
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-02 21:32:18 +0000

    Remove unnecessary TODO
    It's not actually a good idea to make the button type dependent on the
    button list type.

--- HarpGuru/Controls/CovarianceButton/types/index.ts
+++ HarpGuru/Controls/CovarianceButton/types/index.ts
@@ -3,7 +3,6 @@ import type { CovariantsGroup } from 'harpstrata'
 import type { HarpStrataControlProps } from '../../types'
 import type { ControlVarsPrimer } from '../../CovariantsGroupList'
 
-// TODO: this should compose on top of CovarianceButtonListProps
 export type CovarianceButtonProps =
   HarpStrataControlProps
 & Pick<ControlVarsPrimer, 'lockedType' | 'variedType'>
