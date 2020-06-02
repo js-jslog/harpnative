fa21b0d2c5834663d7026cea2bec2cb328105c57
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-02 20:43:37 +0000

    Utility type used to tighten up button props

--- HarpGuru/Controls/CovarianceButton/types/index.ts
+++ HarpGuru/Controls/CovarianceButton/types/index.ts
@@ -4,4 +4,4 @@ import type { HarpStrataControlProps } from '../../types'
 import type { ControlVarsPrimer } from '../../CovariantsGroupList'
 
 // TODO: this should compose on top of CovarianceButtonListProps
-export type CovarianceButtonProps = HarpStrataControlProps & ControlVarsPrimer & CovariantsGroup
+export type CovarianceButtonProps = HarpStrataControlProps & Pick<ControlVarsPrimer, 'lockedType' | 'variedType'> & CovariantsGroup
