abf9c5d84c421d20c70b949fd6edfd1e0e5f813a
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-06 12:27:50 +0000

    Use HarpFaceFacts to size the face

--- HarpGuru/HarpFace/HarpFace/components/index.tsx
+++ HarpGuru/HarpFace/HarpFace/components/index.tsx
@@ -2,6 +2,7 @@ import {StyleSheet, View} from 'react-native'
 import React from 'react'
 
 import type {HarpFaceProps} from '../types'
+import { getHarpFaceFacts } from '../getHarpFaceFacts'
 import { HoleNumberRow } from '../../HoleNumberRow'
 import { getHarpRows } from '../../HarpRows'
 import { themeSizes } from '../../../Styles'
@@ -11,6 +12,8 @@ const { 9: rowHeight } = themeSizes
 
 export const HarpFace = (props: HarpFaceProps): React.ReactElement => {
   const harpRows = getHarpRows(props)
+
+  const { columnCount, rowCount } = getHarpFaceFacts(props)
   const styles = StyleSheet.create({
     facewrapper: {
       flex: 5,
@@ -18,8 +21,8 @@ export const HarpFace = (props: HarpFaceProps): React.ReactElement => {
       justifyContent: 'center',
     },
     face: {
-      width: columnWidth * 10,
-      height: rowHeight * 7,
+      width: columnWidth * columnCount,
+      height: rowHeight * rowCount,
     },
   })
 
