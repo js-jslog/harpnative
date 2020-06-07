7694d9f0d336fe622b91e06664b7be816c91961b
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-07 18:50:09 +0000

    Move the component to a dedicated components folder

--- HarpGuru/Controls/ActivityLegendCell/components/index.tsx
+++ HarpGuru/Controls/ActivityLegendCell/components/index.tsx
@@ -2,7 +2,7 @@ import { Text } from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
 
-import type { ActivityLegendCellProps } from './types'
+import type { ActivityLegendCellProps } from '../types'
 
 export const ActivityLegendCell = (props: ActivityLegendCellProps): ReactElement => {
   const { itemId } = props
