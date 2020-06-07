89350ff11e116542f247f57e7dd773307ff615bd
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-07 11:51:55 +0000

    Remove styles from render function
    This makes it clearer that these particular styles are not dependent on
    the component props.

--- HarpGuru/HarpFace/HoleNumber/components/index.tsx
+++ HarpGuru/HarpFace/HoleNumber/components/index.tsx
@@ -5,11 +5,11 @@ import type { ReactElement } from 'react'
 import type { HoleNumberIds, HoleNumberProps } from '../types'
 import { getStyles } from '../styles'
 
+const styles = getStyles()
+
 export function HoleNumber(props: HoleNumberProps): ReactElement {
   const { xCoord } = props
   const holeNumber: HoleNumberIds = `${xCoord + 1}` as HoleNumberIds
-
-  const styles = getStyles()
 
   return (
     <View style={styles.cell}>
