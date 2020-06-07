d9a6edf8f60d18995cb50f02688a9046c150bf9a
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-07 21:32:03 +0000

    Simplify the column generation code
    There was some difficulty with running the `map` function due to the
    union type. You'll notice that the idSequence function is cast to a
    slightly different union type to the one which is returned from the
    sequence generation function. This is to deal with a type quirk which is
    discussed here:
    https://stackoverflow.com/questions/49510832/typescript-how-to-map-over-union-array-type

--- HarpGuru/Controls/ActivityLegendColumn/components/index.tsx
+++ HarpGuru/Controls/ActivityLegendColumn/components/index.tsx
@@ -1,33 +1,31 @@
 import { View } from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
-import { isPitchId, getPitchIds, DegreeIds } from 'harpstrata'
+import { isPitchId, getPitchIds, DegreeIds, PitchIds } from 'harpstrata'
 
 import type { ActivityLegendColumnProps } from '../types'
 import { ActivityLegendCell } from '../../ActivityLegendCell'
 
-export const ActivityLegendColumn = (props: ActivityLegendColumnProps): ReactElement => {
+const getIdSequence = (props: ActivityLegendColumnProps): ReadonlyArray<DegreeIds> | ReadonlyArray<PitchIds> => {
   const { originId } = props
 
-  if ( isPitchId(originId) ) {
-    const idSequence = getPitchIds(originId)
-    const elementArray = idSequence.map((pitchId, index) => {
-      return <ActivityLegendCell key={index} itemId={pitchId} />
-    })
-    return (
-      <View>
-        { elementArray }
-      </View>
-    )
+  if (isPitchId(originId)) {
+    return getPitchIds(originId)
   } else {
-    const idSequence = Object.values(DegreeIds)
-    const elementArray = idSequence.map((pitchId, index) => {
-      return <ActivityLegendCell key={index} itemId={pitchId} />
-    })
-    return (
-      <View>
-        { elementArray }
-      </View>
-    )
+    return Object.values(DegreeIds)
   }
+}
+
+const mapIdToElement = (itemId: DegreeIds | PitchIds, index: number): ReactElement => {
+  return <ActivityLegendCell key={index} itemId={itemId} />
+}
+
+export const ActivityLegendColumn = (props: ActivityLegendColumnProps): ReactElement => {
+  const idSequence = getIdSequence(props)
+  const elementArray = (idSequence as ReadonlyArray<DegreeIds | PitchIds>).map(mapIdToElement)
+  return (
+    <View>
+      { elementArray }
+    </View>
+  )
 }
