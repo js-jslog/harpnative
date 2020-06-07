732bc62e50f187f760650f3eccc80025c1c3152a
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-07 20:01:40 +0000

    Add test to present pitches as well as degrees

--- HarpGuru/Controls/ActivityLegendCell/components/index.test.tsx
+++ HarpGuru/Controls/ActivityLegendCell/components/index.test.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { DegreeIds } from 'harpstrata'
+import { DegreeIds, PitchIds } from 'harpstrata'
 import { render } from '@testing-library/react-native'
 
 import { ActivityLegendCell } from './index'
 
-test('A component is rendered displaying an itemId', () => {
+test('A component is rendered displaying a DegreeIds itemId', () => {
   const activityLegendCell = {
     itemId: DegreeIds.Root
   }
   const { getByText } = render(<ActivityLegendCell {...activityLegendCell} />)
   expect(getByText(DegreeIds.Root)).toBeTruthy()
+})
+
+test('A component is rendered displaying a PitchIds itemId', () => {
+  const activityLegendCell = {
+    itemId: PitchIds.C
+  }
+  const { getByText } = render(<ActivityLegendCell {...activityLegendCell} />)
+  expect(getByText(PitchIds.C)).toBeTruthy()
 })
