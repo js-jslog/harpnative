c4ca442caff72210dca9dc7a958b9a6bf19c37b8
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-08 16:44:18 +0000

    Add simple styling to make the legend presentablish
    This is just getting it vaguely in the right place. Next comes the real
    tarting up.

--- HarpGuru/Controls/ActivityLegend/components/index.tsx
+++ HarpGuru/Controls/ActivityLegend/components/index.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 import type { ReactElement } from 'react'
 import { DegreeIds, PitchIds } from 'harpstrata'
@@ -11,9 +11,17 @@ const activePitchIds = [ PitchIds.C ]
 const degreeActivityLegendColumnProps = { originId: DegreeIds.Root, activeIds: activeDegreeIds }
 const pitchActivityLegendColumnProps = { originId: PitchIds.C, activeIds: activePitchIds }
 
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingRight: 20,
+  }
+})
+
 export const ActivityLegend = (): ReactElement => {
   return (
-    <View>
+    <View style={styles.wrapper} >
       <ActivityLegendColumn {...degreeActivityLegendColumnProps} />
       <ActivityLegendColumn {...pitchActivityLegendColumnProps} />
     </View>
