d21fdff24910e5b778da32b456a72c502ce55723
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-09 17:11:58 +0000

    Use display mode to determine style of legend cell
    This is looking messy now. There'll be time to refactor it later once
    it's actually looking how we want it and can take a snapshot of it to
    prevent regression.

--- HarpGuru/Controls/ActivityLegendCell/styles/index.ts
+++ HarpGuru/Controls/ActivityLegendCell/styles/index.ts
@@ -4,6 +4,7 @@ import { isPitchId } from 'harpstrata'
 import type { ActivityLegendCellStyles, ActivityLegendCellProps } from '../types'
 import { analyseLegendKey } from '../analyseLegendKey'
 import { themeSizes, themeColors } from '../../../Styles'
+import { DisplayModes } from '../../../HarpFace'
 
 
 const { 7: height, 7: width } = themeSizes
@@ -11,14 +12,15 @@ const { 6: fontSize } = themeSizes
 const { pageColor, degreeColors, inertOutline } = themeColors
 
 export const getStyles = (props: ActivityLegendCellProps): ActivityLegendCellStyles => {
-  const { itemId } = props
+  const { itemId, activeDisplayMode } = props
   const { isActive, counterpartDegreeId } = analyseLegendKey(props)
   const { [counterpartDegreeId]: activeColor } = degreeColors
-  const activeFontColor = (isPitchId(itemId)) ? activeColor : pageColor
+  const isDisplayType = (activeDisplayMode === DisplayModes.Pitch && isPitchId(itemId)) || (activeDisplayMode === DisplayModes.Degree && !isPitchId(itemId))
+  const activeFontColor = (isDisplayType) ? pageColor : activeColor
 
   const styles = StyleSheet.create<ActivityLegendCellStyles>({
     cell: {
-      backgroundColor: (isActive && !isPitchId(itemId)) ? activeColor : pageColor,
+      backgroundColor: (isActive && isDisplayType) ? activeColor : pageColor,
       justifyContent: 'center',
       alignItems: 'center',
       width,
