5aea65e4ecdb2ffd407559fadd05a3bae1a33aad
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-09 07:20:35 +0000

    Base legend key colour on it's position in series

--- HarpGuru/Controls/ActivityLegendCell/styles/index.ts
+++ HarpGuru/Controls/ActivityLegendCell/styles/index.ts
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native'
-import { DegreeIds } from 'harpstrata'
 
 import type { ActivityLegendCellStyles, ActivityLegendCellProps } from '../types'
 import { analyseLegendKey } from '../analyseLegendKey'
@@ -11,12 +10,12 @@ const { 6: fontSize } = themeSizes
 const { pageColor, degreeColors, inertOutline } = themeColors
 
 export const getStyles = (props: ActivityLegendCellProps): ActivityLegendCellStyles => {
-  const { isActive } = analyseLegendKey(props)
-  const { [DegreeIds.Root]: cellColor } = degreeColors
+  const { isActive, counterpartDegreeId } = analyseLegendKey(props)
+  const { [counterpartDegreeId]: activeColor } = degreeColors
 
   const styles = StyleSheet.create<ActivityLegendCellStyles>({
     cell: {
-      backgroundColor: (isActive) ? cellColor : pageColor,
+      backgroundColor: (isActive) ? activeColor : pageColor,
       justifyContent: 'center',
       alignItems: 'center',
       width,
