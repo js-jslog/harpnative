2d758561a3cf80fd905069a07a7c93744445f7aa
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-09 07:34:45 +0000

    Make pitch legend keys highlight only font colour
    The idea here is that when the degrees or pitches in the harpface are
    displayed then their counterpart in the legend will be displayed with
    the background color because then the legend key style represents the
    cell in the harpface. At the moment the legend doesn't have information
    on the display mode so I'm just hard wiring it to appear as though
    Degrees are displayed.

--- HarpGuru/Controls/ActivityLegendCell/styles/index.ts
+++ HarpGuru/Controls/ActivityLegendCell/styles/index.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import { isPitchId } from 'harpstrata'
 
 import type { ActivityLegendCellStyles, ActivityLegendCellProps } from '../types'
 import { analyseLegendKey } from '../analyseLegendKey'
@@ -10,19 +11,21 @@ const { 6: fontSize } = themeSizes
 const { pageColor, degreeColors, inertOutline } = themeColors
 
 export const getStyles = (props: ActivityLegendCellProps): ActivityLegendCellStyles => {
+  const { itemId } = props
   const { isActive, counterpartDegreeId } = analyseLegendKey(props)
   const { [counterpartDegreeId]: activeColor } = degreeColors
+  const activeFontColor = (isPitchId(itemId)) ? activeColor : pageColor
 
   const styles = StyleSheet.create<ActivityLegendCellStyles>({
     cell: {
-      backgroundColor: (isActive) ? activeColor : pageColor,
+      backgroundColor: (isActive && !isPitchId(itemId)) ? activeColor : pageColor,
       justifyContent: 'center',
       alignItems: 'center',
       width,
       height,
     },
     text: {
-      color: (isActive) ? pageColor : inertOutline,
+      color: (isActive) ? activeFontColor : inertOutline,
       fontSize,
     },
   })
