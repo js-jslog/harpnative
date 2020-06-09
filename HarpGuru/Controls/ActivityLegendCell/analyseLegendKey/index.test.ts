1d763dcbad7629c821333e27a9832242bd2a96c6
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-09 07:15:08 +0000

    Add degree input test for counterpart degree function

--- HarpGuru/Controls/ActivityLegendCell/analyseLegendKey/index.test.ts
+++ HarpGuru/Controls/ActivityLegendCell/analyseLegendKey/index.test.ts
@@ -44,3 +44,20 @@ test('Counterpart degree is identified correctly', () => {
   expect(counterpartDegreeIdSecond).toBe(DegreeIds.Second)
   expect(counterpartDegreeIdFlat5).toBe(DegreeIds.Flat5)
 })
+
+test('Counterpart degree functionality works on degree id input by returning that input', () => {
+  const activeHarpStrata = keyCHarpStrata
+  const setActiveHarpStrata = jest.fn()
+  const activeDegreeIds = [ DegreeIds.Root ]
+  const activityLegendCellPropsRoot = { activeHarpStrata, setActiveHarpStrata, itemId: DegreeIds.Root, activeIds: activeDegreeIds }
+  const activityLegendCellPropsSecond = { activeHarpStrata, setActiveHarpStrata, itemId: DegreeIds.Second, activeIds: activeDegreeIds }
+  const activityLegendCellPropsFlat5 = { activeHarpStrata, setActiveHarpStrata, itemId: DegreeIds.Flat5, activeIds: activeDegreeIds }
+
+  const { counterpartDegreeId: counterpartDegreeIdRoot  } = analyseLegendKey(activityLegendCellPropsRoot)
+  const { counterpartDegreeId: counterpartDegreeIdSecond  } = analyseLegendKey(activityLegendCellPropsSecond)
+  const { counterpartDegreeId: counterpartDegreeIdFlat5  } = analyseLegendKey(activityLegendCellPropsFlat5)
+
+  expect(counterpartDegreeIdRoot).toBe(DegreeIds.Root)
+  expect(counterpartDegreeIdSecond).toBe(DegreeIds.Second)
+  expect(counterpartDegreeIdFlat5).toBe(DegreeIds.Flat5)
+})
