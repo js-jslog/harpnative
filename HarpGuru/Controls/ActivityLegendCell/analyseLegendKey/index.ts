718bebe100dfdad2ca3a413003b3aa3b81d732b2
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-09 07:00:07 +0000

    Refactor function for upcomming additional complexity

--- HarpGuru/Controls/ActivityLegendCell/analyseLegendKey/index.ts
+++ HarpGuru/Controls/ActivityLegendCell/analyseLegendKey/index.ts
@@ -4,10 +4,16 @@ type LegendKeyFacts = {
   readonly isActive: boolean;
 }
 
-export const analyseLegendKey = (props: ActivityLegendCellProps): LegendKeyFacts => {
+const getIsActive = (props: ActivityLegendCellProps): boolean => {
   const { itemId, activeIds } = props
 
   const isActive = [...activeIds].includes(itemId)
+
+  return isActive
+}
+
+export const analyseLegendKey = (props: ActivityLegendCellProps): LegendKeyFacts => {
+  const isActive = getIsActive(props)
 
   return { isActive }
 }
