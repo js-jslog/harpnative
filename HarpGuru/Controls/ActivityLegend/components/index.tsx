50a6adf9fa31738f5a4876faa1f418d4bfb68729
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-15 21:51:08 +0000

    Add a gesture dependent animation
    I think this is constantly crossing the bridge though based on the
    performance.

--- HarpGuru/Controls/ActivityLegend/components/index.tsx
+++ HarpGuru/Controls/ActivityLegend/components/index.tsx
@@ -38,13 +38,22 @@ export const ActivityLegend = (props: ActivityLegendProps): ReactElement => {
     if (nativeEvent.translationX < nativeEvent.translationY) setActiveDisplayMode(DisplayModes.Degree)
   }
 
+  const handleGesture = ({nativeEvent}: PanGestureHandlerGestureEvent): void => {
+    const { translationX: x, translationY: y } = nativeEvent
+    const delta = x - y
+    const value = (delta < 100 && delta > -100) ? delta : 0
+    setOffset(value)
+  }
+
 
   const [activity, setActivity] =  useState<0 | 1>(0)
+  const [ offset, setOffset ] = useState(0)
 
   return (
     <>
       <PanGestureHandler
         onHandlerStateChange={handleSwipe}
+        onGestureEvent={handleGesture}
       >
         <View style={styles.wrapper} >
           <ActivityLegendColumn {...degreeActivityLegendColumnProps} />
@@ -53,7 +62,7 @@ export const ActivityLegend = (props: ActivityLegendProps): ReactElement => {
       </PanGestureHandler>
       <ScrollView style={styles.scrollview}>
         <Animated.View style={[styles.switch, {
-          left: activity === 0 ? -50 : 50,
+          transform: [{ translateX: offset }],
         }]} />
         <Button title={'click'} onPress={(): void => {activity === 0 ? setActivity(1) : setActivity(0)}} />
       </ScrollView>
