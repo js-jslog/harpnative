7cd50d41b1c093304d2eb5bb993804a64b6e1186
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-25 16:51:24 +0000

    Remove unused code

--- HarpGuru/Controls/ActivityLegend/components/index.tsx
+++ HarpGuru/Controls/ActivityLegend/components/index.tsx
@@ -1,7 +1,6 @@
 import { useTimingTransition, onGestureEvent } from 'react-native-redash'
 import Animated, { Value, interpolate } from 'react-native-reanimated'
 import { PanGestureHandler, State } from 'react-native-gesture-handler'
-import type { PanGestureHandlerGestureEvent } from 'react-native-gesture-handler'
 import { Button, ScrollView, StyleSheet, View } from 'react-native'
 import React, { useState } from 'react'
 import type { ReactElement } from 'react'
@@ -9,7 +8,6 @@ import { DegreeIds } from 'harpstrata'
 
 import type { ActivityLegendProps } from '../types'
 import { ActivityLegendColumn } from '../../ActivityLegendColumn'
-import { DisplayModes } from '../../../HarpFace'
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -29,16 +27,10 @@ const styles = StyleSheet.create({
 })
 
 export const ActivityLegend = (props: ActivityLegendProps): ReactElement => {
-  const { setActiveDisplayMode, activeHarpStrata, setActiveHarpStrata, activeDisplayMode } = props
+  const { activeHarpStrata, setActiveHarpStrata, activeDisplayMode } = props
   const { rootPitchId, isActiveComplex: { activeDegreeIds, activePitchIds }} = activeHarpStrata
   const degreeActivityLegendColumnProps = { activeHarpStrata, setActiveHarpStrata, originId: DegreeIds.Root, activeIds: activeDegreeIds, activeDisplayMode }
   const pitchActivityLegendColumnProps = { activeHarpStrata, setActiveHarpStrata, originId: rootPitchId, activeIds: activePitchIds, activeDisplayMode }
-
-  const handleSwipe = ({nativeEvent}: PanGestureHandlerGestureEvent): void => {
-    if (nativeEvent.translationX > nativeEvent.translationY) setActiveDisplayMode(DisplayModes.Pitch)
-    if (nativeEvent.translationX < nativeEvent.translationY) setActiveDisplayMode(DisplayModes.Degree)
-  }
-
 
   const state = new Value(State.UNDETERMINED)
   const translationX = new Value(0)
@@ -54,16 +46,11 @@ export const ActivityLegend = (props: ActivityLegendProps): ReactElement => {
   const [slid, setSlid] =  useState<0 | 1>(0)
 
   const transitionFlip = useTimingTransition(flipped, { duration: 400 })
-  const transitionSlide = useTimingTransition(slid, { duration: 400 })
   const rotate = interpolate(transitionFlip, {
     inputRange: [0, 1],
     outputRange: [0, Math.PI / 1]
   })
   const slide = Animated.add(translationX, 100)
-  //const slide = interpolate(transitionSlide, {
-  //  inputRange: [0, 1],
-  //  outputRange: [0, 100]
-  //})
 
   return (
     <View>
