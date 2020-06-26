d5d192a50b14e90ace85692f319112f2c43e404d
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-26 10:09:06 +0000

    Add headsup overlay to harpface
    This is the beginnings of something which might be usable as a
    non-invasive information aid. We need to tell the user what position and
    key they are navigating to, but we don't need it to be constantly on the
    screen.

--- HarpGuru/Screens/Home/index.tsx
+++ HarpGuru/Screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import Animated, {useCode, Value, Clock, startClock, cond, eq, set, interpolate, Extrapolate, add} from 'react-native-reanimated'
 import {StyleSheet, View} from 'react-native'
 import React from 'react'
 
@@ -5,6 +6,7 @@ import type { ScreenProps } from '../types'
 import { themeColors } from '../../Styles'
 import { HarpFace } from '../../HarpFace'
 import { SwipeControlWrapper } from '../../Controls'
+
 
 const { pageColor } = themeColors
 
@@ -16,6 +18,12 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'stretch',
   },
+  headsupOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'red',
+    opacity: 0.5,
+    height: 90,
+  }
 })
 
 
@@ -25,11 +33,40 @@ export const HomeScreen = (props: ScreenProps): React.ReactElement => {
   const swipeControlProps = { activeDisplayMode, setActiveDisplayMode }
   const harpFaceProps = { activeHarpStrata, setActiveHarpStrata, activeDisplayMode }
 
+  const clock = new Clock()
+  const startTime = new Value<number>(0)
+  useCode(() => [
+    startClock(clock),
+    cond(
+      eq(startTime, 0),
+      [ set(startTime, clock) ]
+    ),
+  ],
+  [clock]
+  )
+
+  const position = interpolate(clock, {
+    inputRange: [startTime, add(startTime, 500)],
+    outputRange: [-1000, 1000],
+    extrapolate: Extrapolate.CLAMP,
+  })
+
+
   return (
-    <SwipeControlWrapper {...swipeControlProps}>
-      <View style={styles.guruhome}>
-        <HarpFace {...harpFaceProps} />
-      </View>
-    </SwipeControlWrapper>
+    <>
+      <SwipeControlWrapper {...swipeControlProps}>
+        <View style={styles.guruhome}>
+          <HarpFace {...harpFaceProps} />
+        </View>
+      </SwipeControlWrapper>
+      <Animated.View
+        style={[styles.headsupOverlay, {
+          transform: [
+            { translateX: position }
+          ],
+        }]}
+      >
+      </Animated.View>
+    </>
   )
 }
