381075698a3e9a71f4f952710c0da4ace243a4f2
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-26 11:34:02 +0000

    Move headsup display overlay code in to its component

--- HarpGuru/HeadsupDisplay/components/index.tsx
+++ HarpGuru/HeadsupDisplay/components/index.tsx
@@ -1,13 +1,65 @@
-import {View} from 'react-native'
+import Animated, {Clock, Value, useCode, startClock, cond, eq, set, and, greaterOrEq, add, interpolate, Extrapolate, neq} from 'react-native-reanimated'
+import {StyleSheet, Dimensions} from 'react-native'
 import React from 'react'
 
 import type { HeadsupDisplayProps } from '../types'
 
+const styles = StyleSheet.create({
+  headsupOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'red',
+    opacity: 0.5,
+    height: 90,
+  }
+})
+
+const { width: windowWidth } = Dimensions.get('window')
+const scrollDuration = 200
+const scrollOutDelay = 600
+
 export const HeadsupDisplay = (props: HeadsupDisplayProps): React.ReactElement => {
   const { children } = props
+
+  const clock = new Clock()
+  const inStartTime = new Value<number>(-1)
+  const outStartTime = new Value<number>(-1)
+  useCode(() => [
+    startClock(clock),
+    cond(
+      eq(inStartTime, -1),
+      [ set(inStartTime, clock) ]
+    ),
+    cond(
+      and(eq(outStartTime, -1), greaterOrEq(clock, add(inStartTime, scrollOutDelay))),
+      [ set(outStartTime, clock) ]
+    ),
+  ],
+  [clock]
+  )
+
+  const positionIn = interpolate(clock, {
+    inputRange: [inStartTime, add(inStartTime, scrollDuration)],
+    outputRange: [windowWidth * -1, 0],
+    extrapolate: Extrapolate.CLAMP,
+  })
+  const positionOut = cond(
+    neq(outStartTime, -1),
+    interpolate(clock, {
+      inputRange: [outStartTime, add(outStartTime, scrollDuration)],
+      outputRange: [0, windowWidth],
+      extrapolate: Extrapolate.CLAMP,
+    })
+  )
+
+  const position = add(positionIn, positionOut)
   return (
-    <View>
+    <Animated.View
+      style={[styles.headsupOverlay, {
+        transform: [
+          { translateX: position }
+        ],
+      }]}>
       { children }
-    </View>
+    </Animated.View>
   )
 }
--- HarpGuru/Screens/Home/index.tsx
+++ HarpGuru/Screens/Home/index.tsx
@@ -1,9 +1,9 @@
-import Animated, {useCode, Value, Clock, startClock, cond, eq, set, interpolate, Extrapolate, add, greaterOrEq, neq, and} from 'react-native-reanimated'
-import {StyleSheet, View, Dimensions} from 'react-native'
+import {StyleSheet, View, Text} from 'react-native'
 import React from 'react'
 
 import type { ScreenProps } from '../types'
 import { themeColors } from '../../Styles'
+import { HeadsupDisplay } from '../../HeadsupDisplay'
 import { HarpFace } from '../../HarpFace'
 import { SwipeControlWrapper } from '../../Controls'
 
@@ -17,58 +17,14 @@ const styles = StyleSheet.create({
     backgroundColor: pageColor,
     justifyContent: 'space-between',
     alignItems: 'stretch',
-  },
-  headsupOverlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'red',
-    opacity: 0.5,
-    height: 90,
   }
 })
-
-const { width: windowWidth } = Dimensions.get('window')
-const scrollDuration = 200
-const scrollOutDelay = 600
 
 export const HomeScreen = (props: ScreenProps): React.ReactElement => {
   const { activeHarpStrata, activeDisplayMode, setActiveHarpStrata, setActiveDisplayMode } = props
 
   const swipeControlProps = { activeDisplayMode, setActiveDisplayMode }
   const harpFaceProps = { activeHarpStrata, setActiveHarpStrata, activeDisplayMode }
-
-  const clock = new Clock()
-  const inStartTime = new Value<number>(-1)
-  const outStartTime = new Value<number>(-1)
-  useCode(() => [
-    startClock(clock),
-    cond(
-      eq(inStartTime, -1),
-      [ set(inStartTime, clock) ]
-    ),
-    cond(
-      and(eq(outStartTime, -1), greaterOrEq(clock, add(inStartTime, scrollOutDelay))),
-      [ set(outStartTime, clock) ]
-    ),
-  ],
-  [clock]
-  )
-
-  const positionIn = interpolate(clock, {
-    inputRange: [inStartTime, add(inStartTime, scrollDuration)],
-    outputRange: [windowWidth * -1, 0],
-    extrapolate: Extrapolate.CLAMP,
-  })
-  const positionOut = cond(
-    neq(outStartTime, -1),
-    interpolate(clock, {
-      inputRange: [outStartTime, add(outStartTime, scrollDuration)],
-      outputRange: [0, windowWidth],
-      extrapolate: Extrapolate.CLAMP,
-    })
-  )
-
-  const position = add(positionIn, positionOut)
-
 
   return (
     <>
@@ -77,14 +33,7 @@ export const HomeScreen = (props: ScreenProps): React.ReactElement => {
           <HarpFace {...harpFaceProps} />
         </View>
       </SwipeControlWrapper>
-      <Animated.View
-        style={[styles.headsupOverlay, {
-          transform: [
-            { translateX: position }
-          ],
-        }]}
-      >
-      </Animated.View>
+      <HeadsupDisplay><Text>Test text</Text></HeadsupDisplay>
     </>
   )
 }
