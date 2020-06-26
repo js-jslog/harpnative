5e779e17f3a86e1c1c6b19aba341c089a4ce78cb
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-26 14:33:03 +0000

    Componentise the HUDContent constituent parts
    I don't need to make these in to individual modules just yet. Let's see
    if they get any more complex.

--- HarpGuru/HeadsupDisplay/HUDContent/components/index.tsx
+++ HarpGuru/HeadsupDisplay/HUDContent/components/index.tsx
@@ -24,22 +24,33 @@ const styles = StyleSheet.create({
   }
 })
 
+type ChildProps = {
+  readonly children: React.ReactNode
+}
+
+const Title = ({children}: ChildProps): React.ReactElement => {
+  return <Text style={styles.title}>{children}</Text>
+}
+const Variable = ({children}: ChildProps): React.ReactElement => {
+  return <Text style={styles.variable}>{children}</Text>
+}
+
 export const HUDContent = (props: HUDContentProps): React.ReactElement => {
   const { harpKeyId, pozitionId, rootPitchId } = props
 
   return (
     <View style={styles.wrapper}>
       <View style={styles.column}>
-        <Text style={styles.title}>Harp Key</Text>
-        <Text style={styles.variable}>{harpKeyId}</Text>
+        <Title>Harp Key</Title>
+        <Variable>{harpKeyId}</Variable>
       </View>
       <View style={styles.column}>
-        <Text style={styles.title}>Position</Text>
-        <Text style={styles.variable}>{pozitionId}</Text>
+        <Title>Position</Title>
+        <Variable>{pozitionId}</Variable>
       </View>
       <View style={styles.column}>
-        <Text style={styles.title}>Pozition Key</Text>
-        <Text style={styles.variable}>{rootPitchId}</Text>
+        <Title>Pozition Key</Title>
+        <Variable>{rootPitchId}</Variable>
       </View>
     </View>
   )
