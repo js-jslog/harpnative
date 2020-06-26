c768a827cd3864a296d09c83e1d267f17664880b
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-26 11:21:35 +0000

    Add type to HeadsupDisplay to receive children
    The functionality we are replacing in HomeScreen doesn't actually handle
    children already. This is probably one step ahead of what I should be
    doing before migrating the functionality, but now that I've done it I
    don't think going back is worth the time. I judge this to be low risk.

--- HarpGuru/HeadsupDisplay/components/index.test.tsx
+++ HarpGuru/HeadsupDisplay/components/index.test.tsx
@@ -1,9 +1,11 @@
+import { Text } from 'react-native'
 import React from 'react'
 import { render } from '@testing-library/react-native'
 
 import { HeadsupDisplay } from './index'
 
-test('HeadsupDisplay renders a component', () => {
-  const { container } = render(<HeadsupDisplay />)
-  expect(container).toBeTruthy()
+test('HeadsupDisplay renders a component with children', () => {
+  const testText = 'Test text'
+  const { getByText } = render(<HeadsupDisplay><Text>{testText}</Text></HeadsupDisplay>)
+  expect(getByText(testText)).toBeTruthy()
 })
--- HarpGuru/HeadsupDisplay/components/index.tsx
+++ HarpGuru/HeadsupDisplay/components/index.tsx
@@ -1,9 +1,13 @@
 import {View} from 'react-native'
 import React from 'react'
 
-export const HeadsupDisplay = (): React.ReactElement => {
+import type { HeadsupDisplayProps } from '../types'
+
+export const HeadsupDisplay = (props: HeadsupDisplayProps): React.ReactElement => {
+  const { children } = props
   return (
     <View>
+      { children }
     </View>
   )
 }
