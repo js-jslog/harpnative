f9c87c7a7051cdd0206267eea7b3506bf9c30a86
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-27 21:19:25 +0000

    Add bidirectional replacement helper function
    This will replace the `incrementHarpStrataByPozition` function.

---
+++ HarpGuru/HeadsupDisplay/HUDContent/nudgeHarpStrataByPozition/index.test.ts
@@ -0,0 +1,29 @@
+import {HarpStrataProps, ApparatusIds, PozitionIds, PitchIds, ActiveIds, getHarpStrata} from 'harpstrata'
+
+import {DisplayModes} from '../../../HarpFace'
+
+import { nudgeHarpStrataByPozition } from './index'
+
+const baseHarpStrataProps: HarpStrataProps = {
+  apparatusId: ApparatusIds.MajorDiatonic,
+  pozitionId: PozitionIds.First,
+  harpKeyId: PitchIds.C,
+  activeIds: [] as ActiveIds,
+}
+const cHarpFirstPozitionProps = baseHarpStrataProps
+const cHarpSecondPozitionProps = { ...baseHarpStrataProps, pozitionId: PozitionIds.Second }
+
+const cHarpFirstPozition = getHarpStrata(cHarpFirstPozitionProps)
+const cHarpSecondPozition = getHarpStrata(cHarpSecondPozitionProps)
+
+test('provides incremented HarpStrata by pozition along with root pitch id', () => {
+  const actualIncrementedHarpStrata = nudgeHarpStrataByPozition(cHarpFirstPozition, 'UP', DisplayModes.Degree)
+
+  expect(actualIncrementedHarpStrata).toStrictEqual(cHarpSecondPozition)
+})
+
+test('provides decremented HarpStrata by pozition along with root pitch id', () => {
+  const actualDecrementededHarpStrata = nudgeHarpStrataByPozition(cHarpSecondPozition, 'DOWN', DisplayModes.Degree)
+
+  expect(actualDecrementededHarpStrata).toStrictEqual(cHarpFirstPozition)
+})
---
+++ HarpGuru/HeadsupDisplay/HUDContent/nudgeHarpStrataByPozition/index.ts
@@ -0,0 +1,27 @@
+import { HarpStrata, getPozitionIds, getCovariantSet, getHarpStrata, PozitionIds } from 'harpstrata'
+
+import {DisplayModes} from '../../../HarpFace'
+import {getPropsForHarpStrata} from '../../../Controls/ControlHelpers'
+
+const getNextId = (rootId: PozitionIds, direction: 'UP' | 'DOWN'): PozitionIds => {
+  if (direction === 'UP') {
+    const [ , nextPozitionId ] = getPozitionIds(rootId)
+    return nextPozitionId
+  }
+  const [ previousPozitionId ] = getPozitionIds(rootId).slice(-1)
+  return previousPozitionId
+}
+
+export const nudgeHarpStrataByPozition = (activeHarpStrata: HarpStrata, direction: 'UP' | 'DOWN', displayMode: DisplayModes): HarpStrata => {
+  const { pozitionId, harpKeyId } = activeHarpStrata
+
+  const covariantGroup = getCovariantSet({ pozitionId: getNextId(pozitionId, direction), harpKeyId })
+
+  const nextHarpStrataProps = getPropsForHarpStrata({
+    ...activeHarpStrata,
+    pozitionId: covariantGroup.pozitionId,
+    harpKeyId: covariantGroup.harpKeyId,
+  }, displayMode)
+
+  return getHarpStrata(nextHarpStrataProps)
+}
