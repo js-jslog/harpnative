6833e7d9dc3a2fa530d7c65f252844fa934a99f7
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-29 06:54:47 +0000

    Use OptionContainer for all 3 options

--- HarpGuru/HeadsupDisplay/HUDContent/components/index.tsx
+++ HarpGuru/HeadsupDisplay/HUDContent/components/index.tsx
@@ -1,17 +1,13 @@
-import {PanGestureHandler, PanGestureHandlerGestureEvent, State} from 'react-native-gesture-handler'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 
 import {HUDContentProps} from '../types'
 import {nudgeHarpStrataByRootPitch} from '../nudgeHarpStrataByRootPitch'
 import {nudgeHarpStrataByPozition} from '../nudgeHarpStrataByPozition'
 import {nudgeHarpStrataByHarpKey} from '../nudgeHarpStrataByHarpKey'
-import { themeSizes, themeColors } from '../../../Styles'
+import { themeColors } from '../../../Styles'
 
 import { OptionContainer } from './OptionContainer'
-
-const { 7: variableSize, 8: titleSize } = themeSizes
-const { 8: swipeThreshold } = themeSizes
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -19,88 +15,51 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     backgroundColor: themeColors.inertOutline,
     opacity: 0.5,
-  },
-  column: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  title: {
-    fontSize: titleSize
-  },
-  variable: {
-    fontSize: variableSize
   }
 })
-
-type ChildProps = {
-  readonly children: React.ReactNode
-}
-
-const Title = ({children}: ChildProps): React.ReactElement => {
-  return <Text style={styles.title}>{children}</Text>
-}
-const Variable = ({children}: ChildProps): React.ReactElement => {
-  return <Text style={styles.variable}>{children}</Text>
-}
 
 export const HUDContent = (props: HUDContentProps): React.ReactElement => {
   const { activeHarpStrata, setActiveHarpStrata, activeDisplayMode } = props
   const { harpKeyId, pozitionId, rootPitchId } = activeHarpStrata
 
-  const handleRootPitchSwipe = ({nativeEvent}: PanGestureHandlerGestureEvent) => {
-    if (nativeEvent.state === State.END) {
-      if (nativeEvent.translationY > 0) {
-        setActiveHarpStrata(nudgeHarpStrataByRootPitch(activeHarpStrata, 'UP', activeDisplayMode))
-      } else {
-        setActiveHarpStrata(nudgeHarpStrataByRootPitch(activeHarpStrata, 'DOWN', activeDisplayMode))
-      }
-    }
-  }
-
-  const handleHarpKeySwipe = ({nativeEvent}: PanGestureHandlerGestureEvent) => {
-    if (nativeEvent.state === State.END) {
-      if (nativeEvent.translationY > 0) {
-        setActiveHarpStrata(nudgeHarpStrataByHarpKey(activeHarpStrata, 'UP', activeDisplayMode))
-      } else {
-        setActiveHarpStrata(nudgeHarpStrataByHarpKey(activeHarpStrata, 'DOWN', activeDisplayMode))
-      }
-    }
+  const partiallyAppliedHarpKeyNudgeFunction = (direction: 'UP' | 'DOWN') => {
+    return nudgeHarpStrataByHarpKey(activeHarpStrata, direction, activeDisplayMode)
   }
 
   const partiallyAppliedPozitionNudgeFunction = (direction: 'UP' | 'DOWN') => {
     return nudgeHarpStrataByPozition(activeHarpStrata, direction, activeDisplayMode)
   }
 
-  const optionContainerProps = {
+  const partiallyAppliedRootPitchNudgeFunction = (direction: 'UP' | 'DOWN') => {
+    return nudgeHarpStrataByRootPitch(activeHarpStrata, direction, activeDisplayMode)
+  }
+
+  const harpKeyOptionContainerProps = {
+    title: 'Harp Key',
+    optionId: harpKeyId,
+    nudgeFunction: partiallyAppliedHarpKeyNudgeFunction,
+    setActiveHarpStrata,
+  }
+
+  const pozitionOptionContainerProps = {
     title: 'Position',
     optionId: pozitionId,
     nudgeFunction: partiallyAppliedPozitionNudgeFunction,
     setActiveHarpStrata,
   }
 
+  const rootPitchOptionContainerProps = {
+    title: 'Position Key',
+    optionId: rootPitchId,
+    nudgeFunction: partiallyAppliedRootPitchNudgeFunction,
+    setActiveHarpStrata,
+  }
+
   return (
     <View style={styles.wrapper}>
-      <PanGestureHandler
-        activeOffsetY={swipeThreshold}
-        onHandlerStateChange={handleHarpKeySwipe}
-      >
-        <View style={styles.column}>
-          <Title>Harp Key</Title>
-          <Variable>{harpKeyId}</Variable>
-        </View>
-      </PanGestureHandler>
-      <OptionContainer {...optionContainerProps}/>
-      <PanGestureHandler
-        activeOffsetY={swipeThreshold}
-        onHandlerStateChange={handleRootPitchSwipe}
-      >
-        <View style={styles.column}>
-          <Title>Pozition Key</Title>
-          <Variable>{rootPitchId}</Variable>
-        </View>
-      </PanGestureHandler>
+      <OptionContainer {...harpKeyOptionContainerProps}/>
+      <OptionContainer {...pozitionOptionContainerProps}/>
+      <OptionContainer {...rootPitchOptionContainerProps}/>
     </View>
   )
 }
