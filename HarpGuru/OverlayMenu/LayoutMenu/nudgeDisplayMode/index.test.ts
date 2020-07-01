5a549d59da7f2e115fabe917fea049e9d3900789
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-01 21:05:30 +0000

    Add test to flip from other display mode
    We don't need to a test for direction because it's unused. I will see in
    some upcoming commits whether we can remove it from this function all
    together or whether that will mean it becomes invalid in the context it
    needs to be placed.

--- HarpGuru/OverlayMenu/LayoutMenu/nudgeDisplayMode/index.test.ts
+++ HarpGuru/OverlayMenu/LayoutMenu/nudgeDisplayMode/index.test.ts
@@ -13,10 +13,18 @@ const harpStrataProps: HarpStrataProps = {
 
 const inputHarpStrata = getHarpStrata(harpStrataProps)
 
-test('sets the opposite display mode and returns the input harpstrata', () => {
+test('sets the opposite display mode and returns the input harpstrata when input DisplayMode is Pitch', () => {
   const setActiveDisplayMode = jest.fn()
   const returnedHarpStrata = nudgeDisplayMode(inputHarpStrata, DisplayModes.Pitch, setActiveDisplayMode, 'UP')
 
   expect(setActiveDisplayMode.mock.calls[0][0]).toStrictEqual(DisplayModes.Degree)
+  expect(returnedHarpStrata).toStrictEqual(inputHarpStrata)
+})
+
+test('sets the opposite display mode and returns the input harpstrata when input DisplayMode is Degree', () => {
+  const setActiveDisplayMode = jest.fn()
+  const returnedHarpStrata = nudgeDisplayMode(inputHarpStrata, DisplayModes.Degree, setActiveDisplayMode, 'UP')
+
+  expect(setActiveDisplayMode.mock.calls[0][0]).toStrictEqual(DisplayModes.Pitch)
   expect(returnedHarpStrata).toStrictEqual(inputHarpStrata)
 })
--- HarpGuru/OverlayMenu/LayoutMenu/nudgeDisplayMode/index.ts
+++ HarpGuru/OverlayMenu/LayoutMenu/nudgeDisplayMode/index.ts
@@ -3,7 +3,11 @@ import { HarpStrata } from 'harpstrata'
 import {DisplayModes} from '../../../HarpFace'
 
 export const nudgeDisplayMode = (activeHarpStrata: HarpStrata, displayMode: DisplayModes, setActiveDisplayMode: (arg0: DisplayModes) => void, direction: 'UP' | 'DOWN'): HarpStrata => {
-  setActiveDisplayMode(DisplayModes.Degree)
+  if (displayMode === DisplayModes.Degree) {
+    setActiveDisplayMode(DisplayModes.Pitch)
+  } else {
+    setActiveDisplayMode(DisplayModes.Degree)
+  }
 
   return { ...activeHarpStrata }
 }
