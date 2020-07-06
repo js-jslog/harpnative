453027845367c06dd7171e0c0d44b1c28c8f5bcb
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-06 20:13:48 +0000

    Improve the test description

--- HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.test.ts
+++ HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.test.ts
@@ -1,6 +1,6 @@
 import { partiallyApplyNudgeFunction } from './index'
 
-test('creates a function which returns a function', () => {
+test('returns a function', () => {
   const returnedFunction = partiallyApplyNudgeFunction()
 
   expect(returnedFunction).toBeInstanceOf(Function)
