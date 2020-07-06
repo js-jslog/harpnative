72187f39aaf65b07217427a853d9a88c61c03640
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-06 21:37:08 +0000

    Improve typing slightly
    There's still some linting issues here, but one step at a time.

--- HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.ts
+++ HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.ts
@@ -1,4 +1,4 @@
-export const partiallyApplyNudgeFunction = (nudgeFunction: Function, partialParams: Object): (arg0: 'UP' | 'DOWN') => void => {
+export const partiallyApplyNudgeFunction = (nudgeFunction: (arg0: Object, arg1: 'UP' | 'DOWN') => void, partialParams: Object): (arg0: 'UP' | 'DOWN') => void => {
   return (direction: 'UP' | 'DOWN'): void => {
     nudgeFunction(partialParams, direction)
   }
