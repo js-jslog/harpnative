53592d9af4940f781945774fbf0d057788d3c1e5
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-06 21:42:04 +0000

    Improve typing slightly
    I want to make these Record types specify their generic types so that
    it's clear that the object required by the nudge function is the same as
    the one which is passed along after the nudge function itself.

--- HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.ts
+++ HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.ts
@@ -1,4 +1,4 @@
-export const partiallyApplyNudgeFunction = (nudgeFunction: (arg0: Object, arg1: 'UP' | 'DOWN') => void, partialParams: Object): (arg0: 'UP' | 'DOWN') => void => {
+export const partiallyApplyNudgeFunction = (nudgeFunction: (arg0: Record<string, string>, arg1: 'UP' | 'DOWN') => void, partialParams: Record<string, string>): (arg0: 'UP' | 'DOWN') => void => {
   return (direction: 'UP' | 'DOWN'): void => {
     nudgeFunction(partialParams, direction)
   }
