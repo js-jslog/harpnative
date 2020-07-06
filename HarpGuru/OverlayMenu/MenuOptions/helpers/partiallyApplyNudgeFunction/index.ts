6427f24cf4d5638ecbdaeb2a1e859b90e2c3370c
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-06 21:32:18 +0000

    Make partial application function more richly featured

--- HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.ts
+++ HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.ts
@@ -1,3 +1,5 @@
-export const partiallyApplyNudgeFunction = (): (arg0: 'UP' | 'DOWN') => void => {
-  return (): void => {}
+export const partiallyApplyNudgeFunction = (nudgeFunction: Function, partialParams: Object): (arg0: 'UP' | 'DOWN') => void => {
+  return (direction: 'UP' | 'DOWN'): void => {
+    nudgeFunction(partialParams, direction)
+  }
 }
