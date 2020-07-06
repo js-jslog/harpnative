5cad742f1f8c5e30e1b1f5d627bce94efd64afd9
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-06 22:00:50 +0000

    Improve function signature layout for readibility

--- HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.ts
+++ HarpGuru/OverlayMenu/MenuOptions/helpers/partiallyApplyNudgeFunction/index.ts
@@ -1,4 +1,7 @@
-export const partiallyApplyNudgeFunction = <K extends string, T>(nudgeFunction: (arg0: Record<K, T>, arg1: 'UP' | 'DOWN') => void, partialParams: Record<K, T>): (arg0: 'UP' | 'DOWN') => void => {
+export const partiallyApplyNudgeFunction = <K extends string, T>(
+  nudgeFunction: (arg0: Record<K, T>, arg1: 'UP' | 'DOWN') => void,
+  partialParams: Record<K, T>
+): (arg0: 'UP' | 'DOWN') => void => {
   return (direction: 'UP' | 'DOWN'): void => {
     nudgeFunction(partialParams, direction)
   }
