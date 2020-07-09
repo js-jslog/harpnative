4d454a53ad761aa7ac4340b7074a4b2026fe1ca3
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-09 20:33:52 +0000

    Add `type` specifier to import

--- HarpGuru/Menus/Options/PozitionOption/types/index.ts
+++ HarpGuru/Menus/Options/PozitionOption/types/index.ts
@@ -1,3 +1,3 @@
-import { MenuProps } from '../../../types'
+import type { MenuProps } from '../../../types'
 
 export type PozitionOptionProps = Pick<MenuProps, 'activeHarpStrata' | 'setActiveHarpStrata' | 'activeDisplayMode'>
