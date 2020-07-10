a8a0906ee59b50b822e863f49407ca9db32b78d8
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-10 17:17:45 +0000

    Remove TODO - decided against it
    https://github.com/js-jslog/harpnative/pull/32#discussion_r452959504

--- HarpGuru/HarpFace/HarpCells/index.tsx
+++ HarpGuru/HarpFace/HarpCells/index.tsx
@@ -4,7 +4,6 @@ import type { HarpRowProps } from '../HarpRow'
 import { HarpCell } from '../HarpCell'
 import type { YXCoord } from '../HarpCell'
 
-// TODO: move this to the HarpRow module
 export const getHarpCells = (props: HarpRowProps): React.ReactElement[] => {
   const { yCoord, ...harpCellPropsPart } = props
   const { xRange } = harpCellPropsPart
