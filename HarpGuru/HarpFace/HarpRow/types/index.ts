5eb14bab7b0ef8bbc6566a60ce465cd3b80a8a6f
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-10 14:56:34 +0000

    Add TODO

--- HarpGuru/HarpFace/HarpRow/types/index.ts
+++ HarpGuru/HarpFace/HarpRow/types/index.ts
@@ -2,6 +2,8 @@ import type { ViewStyle } from 'react-native'
 
 import type { HarpCellProps } from '../../HarpCell'
 
+// TODO: this should probably be defined in HarpCell
+// since that's the lowest level at which we use Coords.
 export type Coord = number
 
 export type HarpRowProps = Omit<HarpCellProps, 'yxCoord'> & {
