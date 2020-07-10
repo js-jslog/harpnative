ee662d0117d23b311193e74a6218cf46b5d37b8e
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-10 14:53:51 +0000

    Reintroduce HarpRow tests with new requirements
    These can certainly be improved from a style and maintenance
    perspective, but I'll come to that later.

--- HarpGuru/HarpFace/HarpRow/components/index.test.tsx
+++ HarpGuru/HarpFace/HarpRow/components/index.test.tsx
@@ -7,28 +7,30 @@ import { harpFaceProps } from '../../testResources'
 
 import { HarpRow } from './index'
 
-test.skip('A blow row from a major diatonic harmonica can be rendered', () => {
-  const { getAllByText } = render(<HarpRow {...harpFaceProps} yCoord={2} />)
+// TODO: improve the test data setup here. We need to make the
+// test data composable from the bottom up like the types
+test('The first 3 holes of a blow row from a major diatonic harmonica can be rendered', () => {
+  const { getByText } = render(<HarpRow {...harpFaceProps} yCoord={2} xRange={[0,1,2]} />)
 
-  expect(getAllByText(DegreeIds.Root)[0]).toBeTruthy()
-  expect(getAllByText(DegreeIds.Third)[0]).toBeTruthy()
-  expect(getAllByText(DegreeIds.Fifth)[0]).toBeTruthy()
+  expect(getByText(DegreeIds.Root)).toBeTruthy()
+  expect(getByText(DegreeIds.Third)).toBeTruthy()
+  expect(getByText(DegreeIds.Fifth)).toBeTruthy()
 })
 
-test.skip('A snapshot of a non-home row', () => {
-  const { container } = render(<HarpRow {...harpFaceProps} yCoord={0} />)
+test('A snapshot of a non-home row', () => {
+  const { container } = render(<HarpRow {...harpFaceProps} yCoord={0} xRange={[0,1,2,3,4,5,6,7,8,9]} />)
 
   expect(container).toMatchSnapshot()
 })
 
-test.skip('A snapshot of a blow home row', () => {
-  const { container } = render(<HarpRow {...harpFaceProps} yCoord={2} />)
+test('A snapshot of a blow home row', () => {
+  const { container } = render(<HarpRow {...harpFaceProps} yCoord={2} xRange={[0,1,2,3,4,5,6,7,8,9]} />)
 
   expect(container).toMatchSnapshot()
 })
 
-test.skip('A snapshot of a draw home row', () => {
-  const { container } = render(<HarpRow {...harpFaceProps} yCoord={3} />)
+test('A snapshot of a draw home row', () => {
+  const { container } = render(<HarpRow {...harpFaceProps} yCoord={3} xRange={[0,1,2,3,4,5,6,7,8,9]} />)
 
   expect(container).toMatchSnapshot()
 })
