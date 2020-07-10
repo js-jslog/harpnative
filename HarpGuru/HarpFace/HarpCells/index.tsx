891e282142230884318f0833fb5ff9cf580a34e4
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-10 14:39:18 +0000

    Add TODO

--- HarpGuru/HarpFace/HarpCells/index.tsx
+++ HarpGuru/HarpFace/HarpCells/index.tsx
@@ -4,7 +4,7 @@ import type { HarpRowProps } from '../HarpRow'
 import { HarpCell } from '../HarpCell'
 import type { YXCoord } from '../HarpCell'
 
-
+// TODO: move this to the HarpRow module
 export const getHarpCells = (props: HarpRowProps): React.ReactElement[] => {
   const { yCoord, ...harpCellPropsPart } = props
   const { xRange } = harpCellPropsPart
