006d9c7e8b41dfb213676426519162c91f001b67
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-10 09:02:49 +0000

    Add simple chunker to split harp rows in to chunks
    This is a messy and inflexible solution, but it will do for the proof of
    concept.

--- HarpGuru/HarpFace/HarpCells/index.test.tsx
+++ HarpGuru/HarpFace/HarpCells/index.test.tsx
@@ -1,10 +1,16 @@
 import { harpFaceProps } from '../testResources'
 import type { HarpRowProps } from '../HarpRow'
 
-import { getHarpCells } from './index'
+import { getHarpCells, getChunkedHarpCells } from './index'
 
 test('getHarpCells returns an array of HarpCells, the length of the row', () => {
   const harpRowProps: HarpRowProps = { ...harpFaceProps, yCoord: 0 }
   const harpCells = getHarpCells(harpRowProps)
   expect(harpCells.length).toBe(10)
+})
+
+test('getChunkedHarpCells returns an array of arrays, the flattened length of which is the length of the row', () => {
+  const harpRowProps: HarpRowProps = { ...harpFaceProps, yCoord: 0 }
+  const chunkedHarpCells = getChunkedHarpCells(harpRowProps)
+  expect(chunkedHarpCells.flat().length).toBe(10)
 })
--- HarpGuru/HarpFace/HarpCells/index.tsx
+++ HarpGuru/HarpFace/HarpCells/index.tsx
@@ -19,3 +19,23 @@ export const getHarpCells = (props: HarpRowProps): React.ReactElement[] => {
 
   return harpCells
 }
+
+
+export const getChunkedHarpCells = (props: HarpRowProps): React.ReactElement[][] => {
+  const {
+    activeHarpStrata: { degreeMatrix },
+  } = props
+  const { yCoord } = props
+
+  const [matrixRow] = degreeMatrix
+
+  const chunkedMatrixRow = [ matrixRow.slice(0,2), matrixRow.slice(2,5), matrixRow.slice(5)]
+
+  const chunkedHarpCells = chunkedMatrixRow.map(harpCells => harpCells.map((_, index) => {
+    const yxCoord: YXCoord = [yCoord, index]
+    // TODO: this has one extra props than required (the yCoords is included which is messy)
+    return <HarpCell key={index} {...props} yxCoord={yxCoord} />
+  }))
+
+  return chunkedHarpCells
+}
