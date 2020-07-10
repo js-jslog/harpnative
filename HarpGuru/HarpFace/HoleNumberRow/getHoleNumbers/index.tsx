a11de95eff976537107321917663f528b6c2c0c2
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-10 12:57:16 +0000

    Fragment hole numbers just like the cells

--- HarpGuru/HarpFace/HoleNumberRow/getHoleNumbers/index.tsx
+++ HarpGuru/HarpFace/HoleNumberRow/getHoleNumbers/index.tsx
@@ -2,19 +2,22 @@ import React from 'react'
 import type { ReactElement } from 'react'
 import type { Degree } from 'harpstrata'
 
-import type { HarpFaceProps } from '../../types'
+import type { HarpFaceFragmentProps } from '../../types'
 import { HoleNumber } from '../../HoleNumber'
 import type { HoleNumberProps } from '../../HoleNumber'
 
 export const getHoleNumbers = (
-  harpFaceProps: HarpFaceProps
+  props: HarpFaceFragmentProps
 ): ReactElement[] => {
-  const { activeHarpStrata } = harpFaceProps
+  const { activeHarpStrata } = props
   const { degreeMatrix } = activeHarpStrata
-  const [degreeRow] = degreeMatrix
+  // TODO: probably we can literally just use the xRange or similar param
+  // when we come back to do this properly
+  let [degreeRow] = degreeMatrix
+  if (props.xRange) degreeRow = [ ...degreeRow.slice(props.xRange[0], props.xRange[1])]
 
   return degreeRow.map((_: Degree | undefined, index: number) => {
-    const xCoord = index
+    const xCoord = index + (props.xRange && props.xRange[0] || 0)
     const holeNumberProps: HoleNumberProps = { xCoord }
 
     return <HoleNumber key={index} {...holeNumberProps} />
