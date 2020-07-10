7bd6872cc27582e6141a91ca06afcb111729a5be
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-10 14:46:03 +0000

    Add test to make sure cells array contents is as expected

--- HarpGuru/HarpFace/HarpCells/index.test.tsx
+++ HarpGuru/HarpFace/HarpCells/index.test.tsx
@@ -1,3 +1,6 @@
+import { DegreeIds } from 'harpstrata'
+import { render } from '@testing-library/react-native'
+
 import { harpFaceProps } from '../testResources'
 import type { HarpRowProps } from '../HarpRow'
 
@@ -7,4 +10,17 @@ test('getHarpCells returns an array of HarpCells, the length of the range suppli
   const harpRowProps: HarpRowProps = { ...harpFaceProps, yCoord: 0, xRange: [0,1,2] }
   const harpCells = getHarpCells(harpRowProps)
   expect(harpCells.length).toBe(3)
+})
+
+test('getHarpCells returns an array of HarpCells, with a sample containing the expected values', () => {
+  // This test should be covering the blow row holes 2, 3 and 4 of a major diatonic tuned c harp
+  const harpRowProps: HarpRowProps = { ...harpFaceProps, yCoord: 2, xRange: [1,2,3] }
+  const harpCells = getHarpCells(harpRowProps)
+  const { getByText: getByText_0 } = render(harpCells[0])
+  const { getByText: getByText_1 } = render(harpCells[1])
+  const { getByText: getByText_2 } = render(harpCells[2])
+
+  expect(getByText_0(DegreeIds.Third)).toBeTruthy()
+  expect(getByText_1(DegreeIds.Fifth)).toBeTruthy()
+  expect(getByText_2(DegreeIds.Root)).toBeTruthy()
 })
