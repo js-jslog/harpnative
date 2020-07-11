ee2cccf4df44480589555352a07dd647ac6fa2a1
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-11 16:21:13 +0000

    Add test for transposing with mixed types

--- HarpGuru/HarpFace/HarpFace/helpers/transposeMatrix/index.test.ts
+++ HarpGuru/HarpFace/HarpFace/helpers/transposeMatrix/index.test.ts
@@ -47,6 +47,25 @@ test('transposes well in the presence of undefined', () => {
   expect(transposeMatrix(actualTransposition)).toStrictEqual(doubleRow)
 })
 
+test('transposes well with mixed types', () => {
+  const doubleRow = [
+    [ 1, '2', 3, 4, 5, 6 ],
+    [ 9, 8, '7', 6, undefined, undefined]
+  ]
+  const expectedTransposition = [
+    [ 1, 9 ],
+    [ '2', 8 ],
+    [ 3, '7' ],
+    [ 4, 6 ],
+    [ 5, undefined ],
+    [ 6, undefined ]
+  ]
+  const actualTransposition = transposeMatrix(doubleRow)
+
+  expect(actualTransposition).toStrictEqual(expectedTransposition)
+  expect(transposeMatrix(actualTransposition)).toStrictEqual(doubleRow)
+})
+
 test('transposes an empty array without error', () => {
   const empty = [[]]
 
