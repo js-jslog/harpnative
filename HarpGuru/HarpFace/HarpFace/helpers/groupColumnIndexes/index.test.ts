f467301dcefd2c8bb0174e389d5658d7528ff953
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-11 18:47:53 +0000

    Improve function to handle non-root first holes

--- HarpGuru/HarpFace/HarpFace/helpers/groupColumnIndexes/index.test.ts
+++ HarpGuru/HarpFace/HarpFace/helpers/groupColumnIndexes/index.test.ts
@@ -1,8 +1,17 @@
 import { groupColumnIndexes } from './index'
 
-test('creates octave range indexes for a simple HasRootArray', () => {
+test('groups column indexes for a simple HasRootArray', () => {
   const hasRootArray = [ true, false, false, true, false, false ]
   const expectedRangeIndexes = [ [0, 1, 2], [3, 4, 5]]
+
+  const actualRangeIndexes = groupColumnIndexes(hasRootArray)
+
+  expect(actualRangeIndexes).toStrictEqual(expectedRangeIndexes)
+})
+
+test('groups column indexes for a simple HasRootArray which doesnt start with a root column', () => {
+  const hasRootArray = [ false, true, false, true, false, false ]
+  const expectedRangeIndexes = [ [0], [1, 2], [3, 4, 5]]
 
   const actualRangeIndexes = groupColumnIndexes(hasRootArray)
 
--- HarpGuru/HarpFace/HarpFace/helpers/groupColumnIndexes/index.ts
+++ HarpGuru/HarpFace/HarpFace/helpers/groupColumnIndexes/index.ts
@@ -4,7 +4,7 @@ type ColumnRanges = ReadonlyArray<ColumnRange>
 export const groupColumnIndexes = (hasRootArray: ReadonlyArray<boolean>): ColumnRanges => {
   const incrementArray = Array.from(Array(hasRootArray.length).keys())
   const mapped = hasRootArray.map((element, index) => {
-    if (element) {
+    if (element || index === 0) {
       const nextIndex = hasRootArray.indexOf(true, index+1)
       if (nextIndex === -1) return incrementArray.slice(index)
       return incrementArray.slice(index, nextIndex)
