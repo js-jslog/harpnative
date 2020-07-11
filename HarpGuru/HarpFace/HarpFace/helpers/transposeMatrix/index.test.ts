50355b74e3d2810ea6cbbb9f9935f06d4e306c69
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-11 15:46:15 +0000

    Add another simple test

--- HarpGuru/HarpFace/HarpFace/helpers/transposeMatrix/index.test.ts
+++ HarpGuru/HarpFace/HarpFace/helpers/transposeMatrix/index.test.ts
@@ -1,8 +1,10 @@
 import { transposeMatrix } from './index'
 
-test('transposes a sinlge row to a column', () => {
+test('transposes a sinlge row to a column and back again', () => {
   const singleRow = [[ 1, 2, 3, 4, 5, 6 ]]
   const expectedTransposition = [[1], [2], [3], [4], [5], [6]]
+  const actualTransposition = transposeMatrix(singleRow)
 
-  expect(transposeMatrix(singleRow)).toStrictEqual(expectedTransposition)
+  expect(actualTransposition).toStrictEqual(expectedTransposition)
+  expect(transposeMatrix(actualTransposition)).toStrictEqual(singleRow)
 })
