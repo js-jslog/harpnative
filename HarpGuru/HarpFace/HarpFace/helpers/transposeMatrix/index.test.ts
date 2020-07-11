d453979ef681f430ff7cb97db67b5eed4a39f7ac
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-11 16:06:08 +0000

    Add another test to new function
    I want solid evidence about the functions capabilities since errors here
    may be difficult to detect.

--- HarpGuru/HarpFace/HarpFace/helpers/transposeMatrix/index.test.ts
+++ HarpGuru/HarpFace/HarpFace/helpers/transposeMatrix/index.test.ts
@@ -9,6 +9,25 @@ test('transposes a sinlge row to a column and back again', () => {
   expect(transposeMatrix(actualTransposition)).toStrictEqual(singleRow)
 })
 
+test('transposes a two row matrix to a two column matrix and back again', () => {
+  const doubleRow = [
+    [ 1, 2, 3, 4, 5, 6 ],
+    [ 9, 8, 7, 6, 5, 4 ]
+  ]
+  const expectedTransposition = [
+    [ 1, 9 ],
+    [ 2, 8 ],
+    [ 3, 7 ],
+    [ 4, 6 ],
+    [ 5, 5 ],
+    [ 6, 4 ]
+  ]
+  const actualTransposition = transposeMatrix(doubleRow)
+
+  expect(actualTransposition).toStrictEqual(expectedTransposition)
+  expect(transposeMatrix(actualTransposition)).toStrictEqual(doubleRow)
+})
+
 test('transposes an empty array without error', () => {
   const empty = [[]]
 
