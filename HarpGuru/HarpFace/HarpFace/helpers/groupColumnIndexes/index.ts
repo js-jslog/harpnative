3e362f1573bbf4c45ffe56572b6f2b72226f3afb
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-11 19:47:19 +0000

    Rearange function a little for clarity

--- HarpGuru/HarpFace/HarpFace/helpers/groupColumnIndexes/index.ts
+++ HarpGuru/HarpFace/HarpFace/helpers/groupColumnIndexes/index.ts
@@ -4,12 +4,13 @@ type ColumnRanges = ReadonlyArray<ColumnRange>
 export const groupColumnIndexes = (hasRootArray: ReadonlyArray<boolean>): ColumnRanges => {
   const incrementArray = Array.from(Array(hasRootArray.length).keys())
   const mapped = hasRootArray.map((element, index) => {
-    if (element || index === 0) {
-      const nextIndex = hasRootArray.indexOf(true, index+1)
-      if (nextIndex === -1) return incrementArray.slice(index)
-      return incrementArray.slice(index, nextIndex)
-    }
-    return []
+    if (!element && index !== 0) return []
+
+    const nextIndex = hasRootArray.indexOf(true, index+1)
+
+    if (nextIndex === -1) return incrementArray.slice(index)
+
+    return incrementArray.slice(index, nextIndex)
   })
 
   return mapped.filter(element => element.length > 0)
