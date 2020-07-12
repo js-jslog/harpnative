17b07b229be3c9f55dc83d4f5caf448a4062d1ee
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-12 17:41:54 +0000

    Remove unrequired exports

--- HarpGuru/HarpFace/HarpFace/helpers/index.ts
+++ HarpGuru/HarpFace/HarpFace/helpers/index.ts
@@ -1,5 +1,1 @@
-export { transposeMatrix } from './transposeMatrix'
-export { rowHasRoot } from './rowHasRoot'
-export { groupColumnIndexes } from './groupColumnIndexes'
-export type { ColumnRanges } from './groupColumnIndexes'
 export { getHarpFaceFacts } from './getHarpFaceFacts'
