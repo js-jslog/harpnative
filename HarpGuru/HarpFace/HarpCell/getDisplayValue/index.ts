99e460bb4d17f3a85abb695562871efc205c0f3e
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-12 19:00:49 +0000

    Run prettier

--- HarpGuru/HarpFace/HarpCell/getDisplayValue/index.ts
+++ HarpGuru/HarpFace/HarpCell/getDisplayValue/index.ts
@@ -2,7 +2,10 @@ import { HarpCellProps } from '../types'
 import { analysePosition } from '../analysePosition'
 import { DisplayModes } from '../../types'
 
-type DisplayValueTuple = [ string, string ] | [string, undefined] | [undefined, undefined]
+type DisplayValueTuple =
+  | [string, string]
+  | [string, undefined]
+  | [undefined, undefined]
 
 export const getDisplayValue = (props: HarpCellProps): DisplayValueTuple => {
   const positionFacts = analysePosition(props)
@@ -11,10 +14,13 @@ export const getDisplayValue = (props: HarpCellProps): DisplayValueTuple => {
   const { id: degreeId } = thisDegree || { id: undefined }
   const { id: pitchId } = thisPitch || { id: undefined }
 
-  if ( degreeId === undefined || pitchId === undefined) return [undefined, undefined]
+  if (degreeId === undefined || pitchId === undefined)
+    return [undefined, undefined]
 
-  const [note, ...modifiers] = 
-    activeDisplayMode === DisplayModes.Degree ? degreeId.split('') : pitchId.split('')
+  const [note, ...modifiers] =
+    activeDisplayMode === DisplayModes.Degree
+      ? degreeId.split('')
+      : pitchId.split('')
 
   return [note, modifiers.join('')]
 }
