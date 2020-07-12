f4e08dfebd9f519292855e2f4764c2462eb551a2
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-12 07:44:36 +0000

    Use the harp face facts to split the harp face

--- HarpGuru/HarpFace/HarpFace/components/index.tsx
+++ HarpGuru/HarpFace/HarpFace/components/index.tsx
@@ -3,34 +3,24 @@ import React from 'react'
 
 import type { HarpFaceProps } from '../types'
 import { getStyles } from '../styles'
+import { getHarpFaceFacts } from '../getHarpFaceFacts'
 import { HarpFaceFragment } from '../../HarpFaceFragment'
 
 export const HarpFace = (props: HarpFaceProps): React.ReactElement => {
   const styles = getStyles(props)
-  const harpFaceFragmentProps1 = {
-    ...props,
-    xRange: [0,1,2] as [0,1,2]
-  }
-  const harpFaceFragmentProps2 = {
-    ...props,
-    xRange: [3,4,5] as [3,4,5]
-  }
-  const harpFaceFragmentProps3 = {
-    ...props,
-    xRange: [6,7,8] as [6,7,8]
-  }
-  const harpFaceFragmentProps4 = {
-    ...props,
-    xRange: [9] as [9]
-  }
+  const { octaveColumnGroups } = getHarpFaceFacts(props)
+  const fragments = octaveColumnGroups.map((xRange, index) => {
+    const harpFaceFragmentProps = {
+      ...props,
+      xRange
+    }
+    return <HarpFaceFragment key={index} {...harpFaceFragmentProps} />
+  })
 
   return (
     <View style={styles.facewrapper}>
       <View style={styles.face}>
-        <HarpFaceFragment {...harpFaceFragmentProps1} />
-        <HarpFaceFragment {...harpFaceFragmentProps2} />
-        <HarpFaceFragment {...harpFaceFragmentProps3} />
-        <HarpFaceFragment {...harpFaceFragmentProps4} />
+        {fragments}
       </View>
     </View>
   )
