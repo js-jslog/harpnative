eb18484dd22c58cd12048f0b47de202d191c1a31
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-17 10:13:17 +0000

    Add another test the the set property reducer
    This reassures us that the reducer is typesafe and generic

--- HarpGuru/helpers/setPropertyReducer/index.test.ts
+++ HarpGuru/helpers/setPropertyReducer/index.test.ts
@@ -1,5 +1,9 @@
 import { setPropertyReducer } from './index'
 
-test('the reducer returns the value of its action param', () => {
+test('the reducer returns the value of its action param when number', () => {
   expect(setPropertyReducer(0, {value: 1})).toBe(1)
+})
+
+test('the reducer returns the value of its action param when string', () => {
+  expect(setPropertyReducer('pretest', {value: 'posttest'})).toBe('posttest')
 })
