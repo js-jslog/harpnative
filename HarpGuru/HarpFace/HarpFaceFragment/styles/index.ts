fda09ec231a3f445a9f9b96531fe858c0305ba8b
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-18 18:14:26 +0000

    Remove TODO
    As described at
    https://github.com/js-jslog/harpnative/pull/44#discussion_r456811999
    this is not really understood yet, but for the purpose of this pull
    request I will not take advantage of the obvious non-hook-like behaviour
    of `useGlobal` since it's not clear yet whether we want to make these
    helper functions dependent on a global state.

--- HarpGuru/HarpFace/HarpFaceFragment/styles/index.ts
+++ HarpGuru/HarpFace/HarpFaceFragment/styles/index.ts
@@ -1,4 +1,3 @@
-//import { useGlobal } from 'reactn'
 import { StyleSheet } from 'react-native'
 import type { HarpStrata } from 'harpstrata'
 
@@ -13,12 +12,6 @@ type Props = HarpFaceFragmentProps & {
   readonly setActiveHarpStrata: SetActiveHarpStrata
 }
 export const getStyles = (props: Props): HarpFaceFragmentStyles => {
-  // TODO: understand why the linter seems to think that this is
-  // acceptable and the tests aren't failing. This is not a component
-  // body so I didn't expect this to be a legitimate use of this hook.
-  // Perhaps it's not a hook afterall and I can use it in even more
-  // places than I thought.
-  //const [activeHarpStrata, setActiveHarpStrata] = useGlobal('activeHarpStrata')
   const { activeHarpStrata } = props
   const { columnCount } = getFragmentFacts(props)
   const { rowCount } = getHarpFaceFacts({
