82fad91799baac0814f72a4d41d8cda414de0f3a
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-25 15:00:19 +0000

    Merge the HarpCell useQuizCycle into root useQuizCycle
    This is looking a little hard to read now, but it is better that it's
    all in one place. It just needs a little reorganisation now it's
    working.

--- HarpGuru/components/useQuizCycle/index.ts
+++ HarpGuru/components/useQuizCycle/index.ts
@@ -9,10 +9,28 @@ export const useQuizCycle = (menuState: MenuStates): void => {
   const previousMenuState = usePrevious(menuState, menuState)
   const [quizQuestion] = useGlobal('quizQuestion')
   const [activeExperienceMode] = useGlobal('activeExperienceMode')
+  const [activeHarpStrata] = useGlobal('activeHarpStrata')
   const requestNextQuestion = useDispatch('requestNextQuestion')
+  const revealAnswer = useDispatch('revealAnswer')
+
+  const { isActiveComplex: {activeDegreeIds} } = activeHarpStrata
+  const answerGiven = (activeDegreeIds.length > 0)
+
   useEffect(() => {
     if (menuState === MenuStates.NoMenu && previousMenuState !== menuState) {
       requestNextQuestion()
+    }
+    if (answerGiven && activeExperienceMode === ExperienceModes.Quiz) {
+      const revealTimer = setTimeout(() => {
+        revealAnswer()
+      }, 200)
+      const nextQuestionTimer = setTimeout(() => {
+        requestNextQuestion()
+      }, 1500)
+      return () => {
+        clearTimeout(revealTimer)
+        clearTimeout(nextQuestionTimer)
+      }
     }
     if (activeExperienceMode === ExperienceModes.Quiz) {
       const nextQuestionTimer = setTimeout(() => {
@@ -21,5 +39,5 @@ export const useQuizCycle = (menuState: MenuStates): void => {
       return () => clearTimeout(nextQuestionTimer)
     }
     return
-  }, [quizQuestion, activeExperienceMode, menuState])
+  }, [quizQuestion, activeExperienceMode, menuState, answerGiven])
 }
