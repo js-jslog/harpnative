dbfa8698d856ddeb62a365e01c8daab290988ab9
js-jslog <jhs4jbs@hotmail.co.uk>
2020-07-25 10:05:57 +0000

    Create useQuizCycle hook for HarpCell
    As before, this might not be the right place for this hook in the long
    run. It might be a burden on the numerous cells, but it will do being
    here for now.

--- HarpGuru/HarpFace/HarpCell/components/index.tsx
+++ HarpGuru/HarpFace/HarpCell/components/index.tsx
@@ -10,13 +10,12 @@ import React, { useEffect } from 'react'
 import { IsActiveIds } from 'harpstrata'
 
 import { useStyles } from '../useStyles'
+import { useQuizCycle } from '../useQuizCycle'
 import { usePositionAnalysis } from '../usePositionAnalysis'
 import { useDisplayValue } from '../useDisplayValue'
 import { YXCoord } from '../types'
 import { toggleDegreeIdInHarpStrata } from '../toggleDegreeIdInHarpStrata'
 import { setPozitionRootAtCell } from '../setPozitionRootAtCell'
-import { ExperienceModes } from '../../../helpers/setGlobalReactNState'
-import { usePrevious } from '../../../helpers'
 
 type Props = {
   readonly yxCoord: YXCoord
@@ -24,47 +23,11 @@ type Props = {
 
 export const HarpCell = ({ yxCoord }: Props): React.ReactElement => {
   const [activeHarpStrata, setActiveHarpStrata] = useGlobal('activeHarpStrata')
-  const [activeExperienceMode] = useGlobal('activeExperienceMode')
-  const [quizQuestion] = useGlobal('quizQuestion')
-  const { thisDegreeId, thisPitchId, thisIsActiveId } = usePositionAnalysis(
-    yxCoord
-  )
+  const { thisDegreeId, thisPitchId } = usePositionAnalysis(yxCoord)
   const displayValue = useDisplayValue(yxCoord)
   const styles = useStyles(yxCoord)
-  const previousIsActiveId = usePrevious(thisIsActiveId, IsActiveIds.Inactive)
 
-  const requestNextQuestion = useDispatch('requestNextQuestion')
-  const revealAnswer = useDispatch('revealAnswer')
-
-  useEffect(() => {
-    if (
-      thisIsActiveId === IsActiveIds.Active &&
-      previousIsActiveId === IsActiveIds.Inactive &&
-      activeExperienceMode === ExperienceModes.Quiz
-    ) {
-      const revealTimer = setTimeout(() => {
-        // TODO: why does the test fail if I don't test for
-        // requestNextQuestion first? Is it just because I
-        // haven't mocked that global reducer in the test?
-        if (revealAnswer) {
-          revealAnswer()
-        }
-      }, 200)
-      const nextQuestionTimer = setTimeout(() => {
-        // TODO: why does the test fail if I don't test for
-        // requestNextQuestion first? Is it just because I
-        // haven't mocked that global reducer in the test?
-        if (requestNextQuestion) {
-          requestNextQuestion()
-        }
-      }, 1500)
-      return () => {
-        clearTimeout(revealTimer)
-        clearTimeout(nextQuestionTimer)
-      }
-    }
-    return undefined
-  }, [quizQuestion, thisIsActiveId])
+  useQuizCycle(yxCoord)
 
   const handleTapStateChange = ({
     nativeEvent,
---
+++ HarpGuru/HarpFace/HarpCell/useQuizCycle/index.ts
@@ -0,0 +1,47 @@
+import { useGlobal, useDispatch } from 'reactn'
+import { useEffect } from 'react'
+import { IsActiveIds } from 'harpstrata'
+
+import { usePositionAnalysis } from '../usePositionAnalysis'
+import { YXCoord } from '../types'
+import { ExperienceModes } from '../../../helpers/setGlobalReactNState'
+import { usePrevious } from '../../../helpers'
+
+export const useQuizCycle = (yxCoord: YXCoord): void => {
+  const [activeExperienceMode] = useGlobal('activeExperienceMode')
+  const [quizQuestion] = useGlobal('quizQuestion')
+  const { thisIsActiveId } = usePositionAnalysis(yxCoord)
+  const previousIsActiveId = usePrevious(thisIsActiveId, IsActiveIds.Inactive)
+
+  const requestNextQuestion = useDispatch('requestNextQuestion')
+  const revealAnswer = useDispatch('revealAnswer')
+  useEffect(() => {
+    if (
+      thisIsActiveId === IsActiveIds.Active &&
+      previousIsActiveId === IsActiveIds.Inactive &&
+      activeExperienceMode === ExperienceModes.Quiz
+    ) {
+      const revealTimer = setTimeout(() => {
+        // TODO: why does the test fail if I don't test for
+        // requestNextQuestion first? Is it just because I
+        // haven't mocked that global reducer in the test?
+        if (revealAnswer) {
+          revealAnswer()
+        }
+      }, 200)
+      const nextQuestionTimer = setTimeout(() => {
+        // TODO: why does the test fail if I don't test for
+        // requestNextQuestion first? Is it just because I
+        // haven't mocked that global reducer in the test?
+        if (requestNextQuestion) {
+          requestNextQuestion()
+        }
+      }, 1500)
+      return () => {
+        clearTimeout(revealTimer)
+        clearTimeout(nextQuestionTimer)
+      }
+    }
+    return undefined
+  }, [quizQuestion, thisIsActiveId])
+}
