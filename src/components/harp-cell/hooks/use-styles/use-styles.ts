eef64c377cf67ba4f95a163b241c4c3f8a264d08
js-jslog <jhs4jbs@hotmail.co.uk>
2020-08-13 18:13:33 +0000

    Have the main note remain centered even with a modifier
    This approach is pretty ugly and risky. I would rather find a way to
    just centre the note and offset the modifier independently.

--- src/components/harp-cell/hooks/use-styles/use-styles.ts
+++ src/components/harp-cell/hooks/use-styles/use-styles.ts
@@ -4,7 +4,7 @@ import { IsActiveIds } from 'harpstrata'
 
 import { usePositionAnalysis } from '../use-position-analysis'
 import { HarpCellStyles, YXCoord } from '../../harp-cell'
-import { ExperienceModes } from '../../../../types'
+import { ExperienceModes, DisplayModes } from '../../../../types'
 import { sizes, colors } from '../../../../styles'
 
 const {
@@ -21,12 +21,15 @@ const { pageColor, degreeColors, inertOutline: borderColor } = colors
 
 export const useStyles = (yxCoord: YXCoord): HarpCellStyles => {
   const [activeExperienceMode] = useGlobal('activeExperienceMode')
+  const [activeDisplayMode] = useGlobal('activeDisplayMode')
   const positionFacts = usePositionAnalysis(yxCoord)
-  const { thisDegreeId, thisIsActiveId } = positionFacts
+  const { thisDegreeId, thisPitchId, thisIsActiveId } = positionFacts
   const isActive = thisIsActiveId === IsActiveIds.Active
   const isQuizMode = activeExperienceMode === ExperienceModes.Quiz
   const cellColor =
     isActive && thisDegreeId ? degreeColors[thisDegreeId] : pageColor
+
+  const displayItem = activeDisplayMode === DisplayModes.Degree ? thisDegreeId : thisPitchId
 
   const styles = StyleSheet.create<HarpCellStyles>({
     cell: {
@@ -42,11 +45,13 @@ export const useStyles = (yxCoord: YXCoord): HarpCellStyles => {
       height,
     },
     note: {
+      left: displayItem && displayItem.length > 1 ? modifierFontSize / 4 : 0,
       display: isQuizMode && !isActive ? 'none' : 'flex',
       color: isActive ? pageColor : borderColor,
       fontSize: noteFontSize,
     },
     modifier: {
+      left: modifierFontSize / 5,
       top: modifierTopMargin,
       display: isQuizMode && !isActive ? 'none' : 'flex',
       alignSelf: 'flex-start',
