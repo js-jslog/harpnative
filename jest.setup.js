08ce6840ab71fb9e70fcec0ed2a6ec5580edeeeb
js-jslog <jhs4jbs@hotmail.co.uk>
2020-08-24 18:45:06 +0000

    Conditionally locate gesture handler mocks
    When this project is running in yarn workspaces and the
    react-native-gesture-handler dep is hoisted, the path needs to be
    traversed up to the root. When it's being installed alone, it doesn't.

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,13 @@
+const fs = require('fs')
+
 jest.mock('react-native-reanimated', () =>
   require('react-native-reanimated/mock')
 )
+
+if (fs.existsSync('./node_modules/react-native-gesture-handler/jestSetup.js')) {
+  require('./node_modules/react-native-gesture-handler/jestSetup.js')
+} else if (fs.existsSync('../node_modules/react-native-gesture-handler/jestSetup.js')) {
+  require('../node_modules/react-native-gesture-handler/jestSetup.js')
+} else {
+  throw new Error('Cannot find react-native-gesture-handler jestSetup.js')
+}
